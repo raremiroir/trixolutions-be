61892ea3a002e5b9e3ed275676f8602340b9eb3e
raremiroir <mirostorm@gmail.com>
2022-12-20 16:21:44 +0100

    Add images object for url reference

---
+++ src/lib/components/Base/Media/images.ts
@@ -0,0 +1,84 @@
+const images = {
+   stock: {
+      beslissende_voorsprong: {
+         '400w': 'src/lib/images/stock/Beslissende-Voorsprong_400w_Stock.webp',
+         '600w': 'src/lib/images/stock/Beslissende-Voorsprong_600w_Stock.webp',
+         '800w': 'src/lib/images/stock/Beslissende-Voorsprong_800w_Stock.webp',
+         '1080w': 'src/lib/images/stock/Beslissende-Voorsprong_1080w_Stock.webp'
+      },
+      ideale_teamplayer: {
+         '400w': 'src/lib/images/stock/Ideale-Teamplayer_400w_Stock.webp',
+         '600w': 'src/lib/images/stock/Ideale-Teamplayer_600w_Stock.webp',
+         '800w': 'src/lib/images/stock/Ideale-Teamplayer_800w_Stock.webp',
+         '1080w': 'src/lib/images/stock/Ideale-Teamplayer_1080w_Stock.webp',
+      },
+      kracht_gezonde_teams: {
+         '400w': 'src/lib/images/stock/Kracht-Van-Gezonde-Teams_400w_Stock.webp',
+         '600w': 'src/lib/images/stock/Kracht-Van-Gezonde-Teams_600w_Stock.webp',
+         '800w': 'src/lib/images/stock/Kracht-Van-Gezonde-Teams_800w_Stock.webp',
+         '1080w': 'src/lib/images/stock/Kracht-Van-Gezonde-Teams_1080w_Stock.webp',
+      },
+      lencioni_ctc: {
+         '400w': 'src/lib/images/stock/Lencioni-CtC_400w_Stock.webp',
+         '600w': 'src/lib/images/stock/Lencioni-CtC_600w_Stock.webp',
+         '800w': 'src/lib/images/stock/Lencioni-CtC_800w_Stock.webp',
+         '1080w': 'src/lib/images/stock/Lencioni-CtC_1080w_Stock.webp',
+      },
+      vergaderen_dodelijk: {
+         '400w': 'src/lib/images/stock/Vergaderen-Is-Dodelijk_400w_Stock.webp',
+         '600w': 'src/lib/images/stock/Vergaderen-Is-Dodelijk_600w_Stock.webp',
+         '800w': 'src/lib/images/stock/Vergaderen-Is-Dodelijk_800w_Stock.webp',
+         '1080w': 'src/lib/images/stock/Vergaderen-Is-Dodelijk_1080w_Stock.webp',
+      },
+   },
+   team: {
+      tom: {
+         color: {
+            '400w': 'src/lib/images/team/Tom-Color_400w.webp',
+            '600w': 'src/lib/images/team/Tom-Color_600w.webp',
+            '800w': 'src/lib/images/team/Tom-Color_800w.webp',
+            '1080w': 'src/lib/images/team/Tom-Color_1080w.webp',
+         },
+         bw: {
+            '400w': 'src/lib/images/team/Tom-BW_400w.webp',
+            '600w': 'src/lib/images/team/Tom-BW_600w.webp',
+            '800w': 'src/lib/images/team/Tom-BW_800w.webp',
+            '1080w': 'src/lib/images/team/Tom-BW_1080w.webp',
+         }
+      },
+      tamara: {
+         bw: {
+            '400w':  'src/lib/images/team/Tamara_400w.webp',
+            '600w':  'src/lib/images/team/Tamara_600w.webp',
+            '800w':  'src/lib/images/team/Tamara_800w.webp',
+            '1080w': 'src/lib/images/team/Tamara_1080w.webp',
+         }
+      },
+      roger: {
+         bw: {
+         }
+      },
+      kelly: {
+         bw: {
+         }
+      },
+      christoph: {
+         bw: {
+            '400w':  'src/lib/images/team/Christoph_400w.webp',
+            '600w':  'src/lib/images/team/Christoph_600w.webp',
+            '800w':  'src/lib/images/team/Christoph_800w.webp',
+            '1080w': 'src/lib/images/team/Christoph_1080w.webp',
+         }
+      },
+      frederik: {
+         bw: {
+            '400w':  'src/lib/images/team/Frederik_400w.webp',
+            '600w':  'src/lib/images/team/Frederik_600w.webp',
+            '800w':  'src/lib/images/team/Frederik_800w.webp',
+            '1080w': 'src/lib/images/team/Frederik_1080w.webp',
+         }
+      },
+   },
+}
+
+export default images;
