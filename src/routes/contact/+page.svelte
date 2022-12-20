e78dc3cfab605cbc667738ae854076b991cecfba
raremiroir <mirostorm@gmail.com>
2022-12-20 17:13:30 +0100

    Add main and breadcrumbs

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,16 +1,20 @@
 <script>
-	import SectionWrapper from "$src/lib/components/Base/Wrapper/SectionWrapper.svelte";
+	import Main from "$src/lib/components/Base/Wrapper/Main.svelte";
+import SectionWrapper from "$src/lib/components/Base/Wrapper/SectionWrapper.svelte";
+	import Breadcrumbs from "$src/lib/components/Common/Breadcrumbs/Breadcrumbs.svelte";
 	import Title from "$src/lib/components/Common/Text/Title.svelte";
 
 </script>
 
-<SectionWrapper name="contact-us">
-   <Title type="h1">Contact Us</Title>
-   <!-- lg -->
-   <div class="tabs">
-      <a class="tab tab-lg tab-lifted">Large</a> 
-      <a class="tab tab-lg tab-lifted tab-active">Large</a> 
-      <a class="tab tab-lg tab-lifted">Large</a>
-   </div>
-
-</SectionWrapper>
+<Main>
+   <Breadcrumbs/>
+   <SectionWrapper name="contact-us">
+      <Title type="h1">Contact Us</Title>
+      <!-- lg -->
+      <div class="dui-tabs w-full">
+         <a class="dui-tab dui-tab-lg dui-tab-lifted w-1/2">Large</a> 
+         <a class="dui-tab dui-tab-lg dui-tab-lifted w-1/2 dui-tab-active">Large</a>
+      </div>
+   
+   </SectionWrapper>
+</Main>
