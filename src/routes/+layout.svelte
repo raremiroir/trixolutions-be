1b676db7d066e6e874a379c443f326d25be13030
raremiroir <mirostorm@gmail.com>
2022-12-20 17:13:22 +0100

    Fix layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
 	import Footer from "$src/lib/components/Common/Footer/Footer.svelte";
 	import Navbar from "$src/lib/components/Common/Navbar/Navbar.svelte";
-import "../app.postcss";</script>
+	import "../app.postcss";
+</script>
 
 
 
