fa14e59bcf5de5b9d9efb66630981706b953cab3
raremiroir <mirostorm@gmail.com>
2022-12-20 16:23:33 +0100

    Add contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import SectionWrapper from "$src/lib/components/Base/Wrapper/SectionWrapper.svelte";
+	import Title from "$src/lib/components/Common/Text/Title.svelte";
+
+</script>
+
+<SectionWrapper name="contact-us">
+   <Title type="h1">Contact Us</Title>
+   <!-- lg -->
+   <div class="tabs">
+      <a class="tab tab-lg tab-lifted">Large</a> 
+      <a class="tab tab-lg tab-lifted tab-active">Large</a> 
+      <a class="tab tab-lg tab-lifted">Large</a>
+   </div>
+
+</SectionWrapper>
