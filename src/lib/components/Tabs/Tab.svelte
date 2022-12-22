7750bd08222bd6c6aef5818013d7e0cf07755253
raremiroir <mirostorm@gmail.com>
2022-12-22 03:19:21 +0100

    Add Tabs component

---
+++ src/lib/components/Tabs/Tab.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { getContext } from 'svelte';
+	import { TABS } from './Tabs.svelte';
+
+	const tab = {};
+	const { registerTab, selectTab, selectedTab } = getContext(TABS);
+
+	registerTab(tab);
+</script>
+
+<button 
+	class="
+		w-full border-none
+		transition-all duration-200 ease-out
+		py-2
+		text-xl font-body  
+
+		{ $selectedTab === tab 
+		? `bg-primary/15 shadow-none
+			font-extrabold text-primary-d2` 
+		: `bg-white shadow-md
+			font-semibold text-primary`}" 
+	on:click={() => selectTab(tab)}>
+	<slot />
+</button>
+
+<style>
+
+	.selected {
+		border-bottom: 2px solid teal;
+		color: #333;
+	}
+</style>
---
+++ src/lib/components/Tabs/TabPanel.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { getContext } from 'svelte';
+	import { TABS } from './Tabs.svelte';
+	import { fly, slide } from 'svelte/transition';
+
+	const panel = {};
+	const { registerPanel, selectedPanel } = getContext(TABS);
+
+	registerPanel(panel);
+</script>
+
+{#if $selectedPanel === panel}
+	<div 
+		in:slide={{duration:500}} out:slide={{duration:500}}
+		class="bg-primary/15 shadow-xl
+		font-medium text-primary-d2
+		py-4 px-8">
+		<slot></slot>
+	</div>
+{/if}
