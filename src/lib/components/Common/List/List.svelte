b824b250c4c2333d477bf2e505ed30f8734f64a9
raremiroir <mirostorm@gmail.com>
2022-12-22 03:18:04 +0100

    Add List & ListItem component

---
+++ src/lib/components/Common/List/List.svelte
@@ -0,0 +1,8 @@
+<script>
+   // Add extra classes to component
+   export let klass = ''
+</script>
+
+<ul class="font-lg font-body {klass}">
+   <slot/>
+</ul>
---
+++ src/lib/components/Common/List/ListItem.svelte
@@ -0,0 +1,16 @@
+<script>
+   // Add extra classes to component
+   export let klass = ''
+   // Large text?
+   export let large = false;
+</script>
+
+<li class="
+      list-disc 
+      ml-8 pl-1
+      font-body leading-relaxed 
+      { large ? 'text-lg' : 'text-base' }
+      text-gray-800 
+      {klass}">
+   <slot/>
+</li>
