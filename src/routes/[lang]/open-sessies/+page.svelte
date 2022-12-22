09780c7f389c3facb5c4079cafcb6319e71c47bb
raremiroir <mirostorm@gmail.com>
2022-12-22 03:20:26 +0100

    Fix spacing and add ul li

--- src/routes/[lang]/open-sessies/+page.svelte
+++ src/routes/[lang]/open-sessies/+page.svelte
@@ -36,108 +36,114 @@ import SectionWrapper from "$src/lib/components/Base/Wrapper/SectionWrapper.svel
 
    <SectionWrapper name="open-workshops">
       <Title type="h1" slot="title">Open Sessies</Title>
+      <ul class="m-0 flex flex-col gap-8">
+         <li>
+            <Card direction="row" titleType="h2" titleSmallest>
+               <!-- Image -->
+               <Image 
+                  src="http://via.placeholder.com/640x360" slot="image"
+                  klass="w-full" height="h-80" />
+               <!-- Prepend Inner -->
+               <Tag slot="prepend-inner" small>Online Infosessie</Tag>
+               <!-- Title -->
+               <span slot="title">Gratis Online Open Infosessies</span>
+               <!-- Content -->
+               Een gratis, open infosessie over onze veelzijdige Lencioni 
+               Teamcoaching Opleiding. Aarzel niet en schrijf je in!
+               <!-- Append Inner -->
+               <div class="flex flex-col gap-4" slot="append-inner">
+                  <div class="flex flex-col gap-1">
+                     {#each infosessie_data as session}
+                        <Tag outlined>
+                           {session.date} | {session.start} - {session.end}
+                        </Tag>
+                     {/each}
+                  </div>
 
-      <Card direction="row" titleType="h2" titleSmallest>
-         <!-- Image -->
-         <Image 
-            src="http://via.placeholder.com/640x360" slot="image"
-            klass="w-full" height="h-80" />
-         <!-- Prepend Inner -->
-         <Tag slot="prepend-inner" small>Online Infosessie</Tag>
-         <!-- Title -->
-         <span slot="title">Gratis Online Open Infosessies</span>
-         <!-- Content -->
-         Een gratis, open infosessie over onze veelzijdige Lencioni 
-         Teamcoaching Opleiding. Aarzel niet en schrijf je in!
-         <!-- Append Inner -->
-         <div class="flex flex-col gap-4" slot="append-inner">
-            <div class="flex flex-col gap-1">
-               {#each infosessie_data as session}
-                  <Tag outlined>
-                     {session.date} | {session.start} - {session.end}
-                  </Tag>
-               {/each}
-            </div>
+                  <div class="flex flex-row gap-2">
+                     <Button color="primary" size="md">
+                           Schrijf me in!
+                     </Button>
+                     <Button color="primary" outlined size="md">
+                           Meer Info
+                     </Button>
+                  </div>
+               </div>
+            </Card>
+         </li>
+         <li>
+            <Card direction="row-reverse" titleType="h2" titleSmallest>
+               <!-- Image -->
+               <Image 
+                  src="http://via.placeholder.com/640x360" slot="image"
+                  klass="w-full" height="h-80" />
+               <!-- Prepend Inner -->
+               <div class="flex flex-wrap gap-2" slot="prepend-inner">
+                  <Tag small>Opleiding in Teamcoaching</Tag>
+                  <Tag small>Hybride Traject</Tag>
+               </div>
+               <!-- Title -->
+               <span slot="title">Hybride Lencioni Leertraject</span>
+               <!-- Content -->
+               Een gratis, open infosessie over onze veelzijdige Lencioni 
+               Teamcoaching Opleiding. Aarzel niet en schrijf je in!
+               <!-- Append Inner -->
+               <div class="flex flex-col gap-4" slot="append-inner">
+                  <div class="flex flex-col gap-1">
+                     <Tag small outlined>Leertraject <b>Met</b> Tweedaagse Masterclass</Tag>
+                     <Tag small outlined>Leertraject <b>Zonder</b> Tweedaagse Masterclass</Tag>
+                  </div>
 
-            <div class="flex flex-row gap-2">
-               <Button color="primary" size="md">
-                     Schrijf me in!
-               </Button>
-               <Button color="primary" outlined size="md">
-                     Meer Info
-               </Button>
-            </div>
-         </div>
-      </Card>
+                  <div class="flex flex-row gap-2">
+                     <Button color="primary" size="md">
+                           Schrijf me in!
+                     </Button>
+                     <Button color="primary" outlined size="md">
+                           Meer Info
+                     </Button>
+                  </div>
+               </div>
+            </Card>
+         </li>
+         <li>
+            <Card direction="row" titleType="h2" titleSmallest>
+               <!-- Image -->
+               <Image 
+                  src="http://via.placeholder.com/640x360" slot="image"
+                  klass="max-w-full" height="h-88" />
+               <!-- Prepend Inner -->
+               <div class="flex flex-wrap gap-2" slot="prepend-inner">
+                  <Tag small>Opleiding in Teamcoaching</Tag>
+                  <Tag small>Hybride Traject</Tag>
+               </div>
+               <!-- Title -->
+               <span slot="title">Lencioni DeepDive / <br/>Advanced Opleiding</span>
+               <!-- Content -->
+               Een gratis, open infosessie over onze veelzijdige Lencioni 
+               Teamcoaching Opleiding. Aarzel niet en schrijf je in!
+               <!-- Append Inner -->
+               <div class="flex flex-col gap-4" slot="append-inner">
+                  <div class="flex flex-col gap-1">
+                     {#each deepdive_data as session}
+                        <Tag outlined>
+                           {session.date} | {session.start} - {session.end}
+                        </Tag>
+                     {/each}
+                  </div>
 
-      <Card direction="row-reverse" titleType="h2" titleSmallest>
-         <!-- Image -->
-         <Image 
-            src="http://via.placeholder.com/640x360" slot="image"
-            klass="w-full" height="h-80" />
-         <!-- Prepend Inner -->
-         <div class="flex flex-wrap gap-2" slot="prepend-inner">
-            <Tag small>Opleiding in Teamcoaching</Tag>
-            <Tag small>Hybride Traject</Tag>
-         </div>
-         <!-- Title -->
-         <span slot="title">Hybride Lencioni Leertraject</span>
-         <!-- Content -->
-         Een gratis, open infosessie over onze veelzijdige Lencioni 
-         Teamcoaching Opleiding. Aarzel niet en schrijf je in!
-         <!-- Append Inner -->
-         <div class="flex flex-col gap-4" slot="append-inner">
-            <div class="flex flex-col gap-1">
-               <Tag small outlined>Leertraject <b>Met</b> Tweedaagse Masterclass</Tag>
-               <Tag small outlined>Leertraject <b>Zonder</b> Tweedaagse Masterclass</Tag>
-            </div>
-
-            <div class="flex flex-row gap-2">
-               <Button color="primary" size="md">
-                     Schrijf me in!
-               </Button>
-               <Button color="primary" outlined size="md">
-                     Meer Info
-               </Button>
-            </div>
-         </div>
-      </Card>
-
-      <Card direction="row" titleType="h2" titleSmallest>
-         <!-- Image -->
-         <Image 
-            src="http://via.placeholder.com/640x360" slot="image"
-            klass="max-w-full" height="h-80" />
-         <!-- Prepend Inner -->
-         <div class="flex flex-wrap gap-2" slot="prepend-inner">
-            <Tag small>Opleiding in Teamcoaching</Tag>
-            <Tag small>Hybride Traject</Tag>
-         </div>
-         <!-- Title -->
-         <span slot="title">Lencioni DeepDive / <br/>Advanced Opleiding</span>
-         <!-- Content -->
-         Een gratis, open infosessie over onze veelzijdige Lencioni 
-         Teamcoaching Opleiding. Aarzel niet en schrijf je in!
-         <!-- Append Inner -->
-         <div class="flex flex-col gap-4" slot="append-inner">
-            <div class="flex flex-col gap-1">
-               {#each deepdive_data as session}
-                  <Tag outlined>
-                     {session.date} | {session.start} - {session.end}
-                  </Tag>
-               {/each}
-            </div>
-
-            <div class="flex flex-row gap-2">
-               <Button color="primary" size="md">
-                     Schrijf me in!
-               </Button>
-               <Button color="primary" outlined size="md">
-                     Meer Info
-               </Button>
-            </div>
-         </div>
-      </Card>
+                  <div class="flex flex-row gap-2">
+                     <Button color="primary" size="md">
+                           Schrijf me in!
+                     </Button>
+                     <Button color="primary" outlined size="md">
+                           Meer Info
+                     </Button>
+                  </div>
+               </div>
+            </Card>
+         </li>
+      </ul>
+      
 
    </SectionWrapper>
 </Main>
