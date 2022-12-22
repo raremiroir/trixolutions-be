b6a6be086e87904acd8c3b1db1bbf71bef5af3ba
raremiroir <mirostorm@gmail.com>
2022-12-22 12:56:09 +0100

    Add hero flexibility

--- src/lib/components/Hero/Hero.svelte
+++ src/lib/components/Hero/Hero.svelte
@@ -3,8 +3,11 @@
 <!--      -->
 <script>
    // Import Components
-   import Title    from '$comp/Common/Text/Title.svelte';
-	import Image from '../Base/Media/Image.svelte';
+   import Title      from '$comp/Common/Text/Title.svelte';
+	import Image      from '$comp/Base/Media/Image.svelte';
+	import Slider     from '$comp/Other/Slider/Slider.svelte';
+	import Slide      from '$comp/Other/Slider/Slide.svelte';
+	import HeroInner  from '$comp/Hero/HeroInner.svelte';
    
    // Import Image
    import IMGw400    from '$img/stock/Kracht-Van-Gezonde-Teams_400w_Stock.webp';
@@ -12,9 +15,6 @@
    import IMGw800    from '$img/stock/Kracht-Van-Gezonde-Teams_800w_Stock.webp';
    import IMGw1080   from '$img/stock/Kracht-Van-Gezonde-Teams_1080w_Stock.webp';
    
-   
-   // Set title
-   export let title = 'De Kracht van Gezonde Teams';
    // Set image sources
    export let img400 = IMGw400;
    export let img600 = IMGw600;
@@ -22,10 +22,21 @@
    export let img1080 = IMGw1080;
    // Set image alt
    export let imgAlt = 'No Alt';
+   // Set image position
+   export let imgPos = 'object-center';
+
    // Set height of hero 
    export let height = '';
 
+   // Using slides?
+   export let slider = false;
 
+   // Set title size
+	export let titleSmall = false;
+	export let titleSmaller = false;
+	export let titleSmallest = false;
+	// Set title type
+	export let titleType = 'h1'
 	
 </script>
 
@@ -34,31 +45,19 @@
 <!--      -->
 
 <div class="w-full h-fit relative z-0">
-   
-   <Image 
-      w400={img400} w600={img600} w800={img800} w1080={img1080}
-      alt="{imgAlt}"
-      klass="object-cover object-center w-full {height ? height : 'h-156 lg:h-160'}"
-   />
-
-   <div class="
-         w-full h-full 
-         bg-black/50 mix-blend-multiply
-         absolute top-0
-         flex items-center">
-      <br/>
-   </div>
-   <div class="
-         w-full h-full 
-         absolute top-0
-         pt-20
-         flex items-center">
-      <div class="w-3/4 mx-auto flex flex-col gap-4">
-         <Title type="h1" color="text-gray-100/90">
-            {title}
-         </Title>
+   {#if slider}
+      <Slider>
          <slot/>
-      </div>
-   </div>
+      </Slider>
+   {:else}
+      <HeroInner 
+         {img400} {img600} {img800} {img1080} 
+         {imgAlt} {height} {imgPos}
+         {titleType} {titleSmall} {titleSmaller} {titleSmallest}>
+         <slot name="title" slot="title">Title</slot>
+         <slot/>
+      </HeroInner>
+   {/if}
+   
 
 </div>
