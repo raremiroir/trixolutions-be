18768f29ec2e5371b7be452dc133bb171daefff9
raremiroir <mirostorm@gmail.com>
2022-12-26 14:51:22 +0100

    Add possibility for title to list

--- src/lib/components/Common/List/List.svelte
+++ src/lib/components/Common/List/List.svelte
@@ -1,8 +1,14 @@
 <script>
    // Add extra classes to component
    export let klass = ''
+   export let title = false;
 </script>
 
-<ul class="font-lg font-body {klass}">
-   <slot/>
-</ul>
+<div class="{klass} w-full">
+   {#if title}
+      <slot name="title"/>
+   {/if}
+   <ul class="font-lg font-body">
+      <slot/>
+   </ul>
+</div>
