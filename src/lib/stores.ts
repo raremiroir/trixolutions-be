5877dfde2b4dd3b7a0f13f88c15dd81b5d368eb0
raremiroir <mirostorm@gmail.com>
2022-12-28 00:34:45 +0100

    Create stores file for storing page results

---
+++ src/lib/stores.ts
@@ -0,0 +1,11 @@
+import { writable } from 'svelte/store'
+
+
+export const status = writable(false);
+export const username = writable("");
+export const user = writable({});
+export const admin = writable({});
+export const verified = writable();
+
+export const pageResult = writable([]);
+export const secondPageResult = writable([]);
