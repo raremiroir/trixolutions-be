0f09ef25b9202f4031c6a5b2d29d126efa1924cc
raremiroir <mirostorm@gmail.com>
2022-12-28 00:36:14 +0100

    Fix Cta

--- src/lib/components/Core/CtA/CtA.svelte
+++ src/lib/components/Core/CtA/CtA.svelte
@@ -8,16 +8,28 @@ import Button from "../Button/Button.svelte";
 
 <div class="w-full flex flex-row items-center justify-center h-fit pt-2 pb-16 divide-x-4 divide-primary/30 divide-dashed">
    <div class="w-1/2 flex items-center justify-center">
-      <Button size="w-2/3 h-full p-6 flex items-center justify-center text-3xl" block>
+      <Button 
+         size="
+            flex items-center justify-center 
+            w-fit lg:w-2/3 h-full 
+            text-lg md:text-xl lg:text-3xl
+            py-3 px-2 lg:py-5 lg:px-4
+            " 
+         block>
          Contacteer Ons!
       </Button>
    </div>
    <div class="w-1/2 flex items-center justify-center flex-col gap-4">
-      <div class="w-2/3 text-justify">
-         <Title type="h4" small klass="text-justify" >Blijf Altijd op de Hoogte!</Title>
-         <Title type="subtitle" small klass="text-justify">Abonneer op onze Trixolutions Nieuwsbrief!</Title>
+      <div class="w-2/3 text-center">
+         <Title type="h4" small >Blijf Altijd op de Hoogte!</Title>
+         <Title type="subtitle" small>Abonneer op onze Trixolutions Nieuwsbrief!</Title>
       </div>
-      <Button size="text-center py-4 px-12">
+      <Button 
+         size="
+            text-center 
+            text-lg md:text-xl lg:text-3xl
+            py-3 px-2 lg:py-5 lg:px-4
+            ">
          Schrijf Je In!
       </Button>
    </div>
