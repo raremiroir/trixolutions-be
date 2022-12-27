300c7790e5220b56a1bc1da700c71ba3084154bd
raremiroir <mirostorm@gmail.com>
2022-12-28 00:38:58 +0100

    Add blog page

---
+++ src/routes/[lang]/(main)/blog/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Main from '$src/lib/components/Base/Wrapper/Main.svelte';
+	import SectionWrapper from '$src/lib/components/Base/Wrapper/SectionWrapper.svelte';
+	import Title from '$src/lib/components/Common/Text/Title.svelte';
+	import Breadcrumbs from '$src/lib/components/Core/Breadcrumbs/Breadcrumbs.svelte';
+    
+</script>
+
+<Breadcrumbs/>
+<Main cta>
+   <SectionWrapper name="blog">
+      <Title slot="title" type='h1'>Trixolutions Blog</Title>
+      
+   </SectionWrapper>
+</Main>
