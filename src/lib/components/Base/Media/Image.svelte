6d4daa4a476cd9144f038757dfce011599fce87d
raremiroir <mirostorm@gmail.com>
2023-01-01 15:39:07 +0100

    Add images

--- src/lib/components/Base/Media/Image.svelte
+++ src/lib/components/Base/Media/Image.svelte
@@ -2,14 +2,6 @@
    // IMPORT IMAGES
       // placeholder
       import placehold  from '$img/placeholder_green.png?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
-      // stock
-      import BV         from  '$img/stock/Beslissende_Voorsprong_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
-      import BV2        from  '$img/stock/Beslissende_Voorsprong_2_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
-      import ITP        from  '$img/stock/Ideale_Teamplayer_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
-      import KGT        from  '$img/stock/Kracht_Van_Gezonde_Teams_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
-      import CTC        from  '$img/stock/Lencioni_CtC_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
-      import VD         from  '$img/stock/Vergaderen_Is_Dodelijk_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
-      import WG         from  '$img/stock/Working_Genius_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
       // lencioni assets
       import pyramid    from '$img/lencioni/pyramide/pyramid-lencioni-1.png?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
       // team
@@ -18,11 +10,52 @@
       import christoph  from '$img/team/Christoph_1080w.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
       import frederik   from '$img/team/Frederik_1080w.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
       import tamara     from '$img/team/Tamara_1080w.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
-      
+      // stock
+      import BV         from  '$img/stock/Beslissende_Voorsprong_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
+      import BV2        from  '$img/stock/Beslissende_Voorsprong_2_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
+      import ITP        from  '$img/stock/Ideale_Teamplayer_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
+      import KGT        from  '$img/stock/Kracht_Van_Gezonde_Teams_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
+      import CTC        from  '$img/stock/Lencioni_CtC_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
+      import VD         from  '$img/stock/Vergaderen_Is_Dodelijk_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
+      import WG         from  '$img/stock/Working_Genius_Stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset';
+      // blog post stock photos
+      import BP1        from '$img/stock/blog/1_vertrouwen_bakermat_succes-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP2        from '$img/stock/blog/2_laat_haters_haten-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP3        from '$img/stock/blog/3_reboarding_you_said_what-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP4        from '$img/stock/blog/4_maak_je_eigen_elearning_articulate-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP5        from '$img/stock/blog/5_beslissingen_nemen_met_team-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP7        from '$img/stock/blog/7_managen_virtuele_teams-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP8        from '$img/stock/blog/8_sturen_op_output_virtuele_teams-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP9        from '$img/stock/blog/9_digitale_oplossing_2_time_management.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP10       from '$img/stock/blog/10_lencioni_herkenbaar_uitbreidbaar_zeker-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP12       from '$img/stock/blog/12_digitale_softskills_opleidingen_trixolutions-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP14       from '$img/stock/blog/14_waarom_psychologische_veiligheid_belangrijker_is_dan_ooit-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP15       from '$img/stock/blog/15_een_rotte_appel_destructieve_teamleden-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP17       from '$img/stock/blog/17_zelforganisatie_iedereen_niemand_manager-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP19       from '$img/stock/blog/19_adviseur_klant_vraagstuk-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP20       from '$img/stock/blog/20_samenwerken_aandacht_persoonlijke_relaties-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP21       from '$img/stock/blog/21_nieuw_in_team_eerst_aanpassen_dan_afwijken-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP22       from '$img/stock/blog/22_waarom_aanspreken_moeilijker_als_je_leidinggeeft-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP24       from '$img/stock/blog/24_hoe_niet_te_delegeren-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP25       from '$img/stock/blog/25_rijkelijk_belonen_weinig_effect_op_kwaliteit-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP26       from '$img/stock/blog/26_hoe_gebruik_je_content_voor_sales-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP27       from '$img/stock/blog/27_zonder_oogcontact_geen_contact-stock.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP28       from '$img/stock/blog/28_6_starters_voor_geweldige_presentatie.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP29       from '$img/stock/blog/29_leer_eerst_jezelf_leiden_dan_anderen.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
+      import BP30       from '$img/stock/blog/30_vijflagenmodel_business_intelligence.webp?w=1536;1280;768;640&format=webp;avif;jpeg&srcset'
       
       export let imgSrc = 'placeholder';
       let convertedSrc;
       switch (imgSrc) {
+         
+         case 'tom_color':                convertedSrc = tom_color;  break;
+         case 'tom':                      convertedSrc = tom;        break;
+         case 'christoph':                convertedSrc = christoph;  break;
+         case 'frederik':                 convertedSrc = frederik;   break;
+         case 'tamara':                   convertedSrc = tamara;     break;
+         
+         case 'lencioni_pyramid':         convertedSrc = pyramid;    break;
+         
          case 'Beslissende_Voorsprong':   convertedSrc = BV;         break;
          case 'Beslissende_Voorsprong_2': convertedSrc = BV2;        break;
          case 'Ideale_Teamplayer':        convertedSrc = ITP;        break;
@@ -31,13 +64,30 @@
          case 'Vergaderen_Is_Dodelijk':   convertedSrc = VD;         break;
          case 'Working_Genius':           convertedSrc = WG;         break;
          
-         case 'tom_color':                convertedSrc = tom_color;  break;
-         case 'tom':                      convertedSrc = tom;        break;
-         case 'christoph':                convertedSrc = christoph;  break;
-         case 'frederik':                 convertedSrc = frederik;   break;
-         case 'tamara':                   convertedSrc = tamara;     break;
-
-         case 'lencioni_pyramid':         convertedSrc = pyramid;    break;
+         case 'vertrouwen_bakermat_succes':  convertedSrc = BP1;         break;
+         case 'laat_haters_haten':           convertedSrc = BP2;         break;
+         case 'reboarding_what':             convertedSrc = BP3;         break;
+         case 'maak_eigen_elearning':        convertedSrc = BP4;         break;
+         case 'beslissingen_nemen_team':     convertedSrc = BP5;         break;
+         case 'managen_virtuele_teams':      convertedSrc = BP7;         break;
+         case 'sturen_op_output':            convertedSrc = BP8;         break;
+         case 'oplossing_time_management':   convertedSrc = BP9;         break;
+         case 'lencioni_herkenbaar_zeker':   convertedSrc = BP10;         break;
+         case 'softskills_opleidingen':      convertedSrc = BP12;         break;
+         case 'pv_belangrijker_dan_ooit':    convertedSrc = BP14;         break;
+         case 'rotte_appel_destructief':     convertedSrc = BP15;         break;
+         case 'iedereen_niemand_manager':    convertedSrc = BP17;         break;
+         case 'adviseur_klant_vraagstuk':    convertedSrc = BP19;         break;
+         case 'aandacht_persoonlijke_rel':   convertedSrc = BP20;         break;
+         case 'aanpassen_dan_afwijken':      convertedSrc = BP21;         break;
+         case 'aanspreken_moeilijker':       convertedSrc = BP22;         break;
+         case 'niet_delegeren':              convertedSrc = BP24;         break;
+         case 'beloning_effect_kwaliteit':   convertedSrc = BP25;         break;
+         case 'content_voor_sales':          convertedSrc = BP26;         break;
+         case 'oogcontact_contact':          convertedSrc = BP27;         break;
+         case 'geweldige_presentatie_start': convertedSrc = BP28;         break;
+         case 'zelf_leiden_dan_anderen':     convertedSrc = BP29;         break;
+         case 'model_business_intelligence': convertedSrc = BP30;         break;
          
          case 'placeholder':              convertedSrc = placehold;  break;
          default: convertedSrc = imgSrc; break;
