c55806cbf5a78144badbc71d9d15cedc1fcbddde
raremiroir <mirostorm@gmail.com>
2023-01-02 16:00:12 +0100

    Fix data handling

--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -18,20 +18,20 @@
    const blogPost = Object(data.post);
    // console.log(blogPost);
 
-   const url = formatUrl(blogPost.title_nl);
+   const url = formatUrl(blogPost.title);
 </script>
 
 <header>
-   <Hero height="h-92" imgSrc="{blogPost.expand.img.name}" imgAlt="{blogPost.title_nl}">
+   <Hero height="h-92" imgSrc="{blogPost.img}" imgAlt="{blogPost.title}">
       <div slot="title">
          <Title type="subheader" color="text-gray-200">Trixolutions Blog</Title>
-         <Title type="h1" color="text-gray-100">{titleCase(blogPost.title_nl)}</Title>
+         <Title type="h1" color="text-gray-100">{titleCase(blogPost.title)}</Title>
          <div class="flex flex-row justify-between w-full items-center">
             <Title type="subtitle" color="text-gray-100/50">
-               {blogPost.expand.author.first_name} {blogPost.expand.author.last_name}
+               {blogPost.author.first_name} {blogPost.author.last_name}
             </Title>
             <Tag outlined klass="float-right text-gray-50/70 border-gray-50/70">
-               {formatDateMonthFull(blogPost.updated)} ({formatTime(blogPost.updated)})
+               {formatDateMonthFull(blogPost.created_at)} ({formatTime(blogPost.created_at)})
             </Tag>
          </div>
       </div>
@@ -41,8 +41,8 @@
 <Main cta partnership>
    <Breadcrumbs/>
       <SectionWrapper name="{url}">
-            <Title type="h2" slot="title">{titleCase(blogPost.title_nl)}</Title>
-            <Markup content={blogPost.content_nl} image="{blogPost.expand.img.name}" imgAlt={blogPost.title_nl}>
+            <Title type="h2" slot="title">{titleCase(blogPost.title)}</Title>
+            <Markup content={blogPost.content} image="{blogPost.img}" imgAlt={blogPost.title}>
             </Markup>
       </SectionWrapper>
 </Main>
