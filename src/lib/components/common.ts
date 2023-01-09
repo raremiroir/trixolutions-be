1f60066f46051d1e9211026d04bfc0ae3d1e661b
raremiroir <mirostorm@gmail.com>
2023-01-09 01:48:08 +0100

    Add new components to export

--- src/lib/components/common.ts
+++ src/lib/components/common.ts
@@ -2,7 +2,8 @@ export { default as Flipper }    from './Common/Flipper.svelte';
 
 export { default as Alert }      from './Common/Alert.svelte';
 
-export { default as Modal }      from './Common/Modal.svelte';
+export { default as Modal }      from './Common/Modal/Modal.svelte';
+export { default as ExternalModal }      from './Common/Modal/ExternalModal.svelte';
 
 export { default as Card }       from './Common/Card.svelte';
 
@@ -12,8 +13,11 @@ export { default as CtA }        from './Common/CtA.svelte';
 
 export { default as Hero }       from './Common/Hero/Hero.svelte';
 export { default as HeroInner }  from './Common/Hero/HeroInner.svelte';
+export { default as HeroSlide }  from './Common/Hero/HeroSlide.svelte';
 
 export { default as Logo }       from './Common/Media/Logo.svelte'
 export { default as Map }        from './Common/Media/Map.svelte'
 export { default as TwicPic }    from './Common/Media/TwicPic.svelte'
 export { default as Video }      from './Common/Media/Video.svelte'
+
+export { default as SlideChangeBtn } from './Common/Slider/SlideChangeBtn.svelte'
