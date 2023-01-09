6301c1e802d4e97cd27cafb78bfc4e3ad7f4186e
raremiroir <mirostorm@gmail.com>
2023-01-09 01:59:21 +0100

    Add author img

--- src/lib/components/Posts/PostScroll.svelte
+++ src/lib/components/Posts/PostScroll.svelte
@@ -4,13 +4,18 @@
 
    import { formatUrl } from "$utils/formatText";
    export let pageData:any;
-      
+
+   
+      export let blog = false;
 </script>
 
 
 <PostGrid>
    {#each pageData as blogPost}
       <PostCard
+         authorImg={blog ? `team/${blogPost.author.img}.webp` : ''}
+         author={blog ? `${blogPost.author.first_name} ${blogPost.author.last_name}` : ''} 
+         date={blog ? blogPost.created_at : ''}
          imgSrc={`${blogPost.img.folder}/${blogPost.img.name}.${blogPost.img.type}`}
          slug={`./blog/${formatUrl(blogPost.title)}`}
          title={blogPost.title}
