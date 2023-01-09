ec2c62cc2969ff510b52711f829313bcdf3923a2
raremiroir <mirostorm@gmail.com>
2023-01-09 01:56:14 +0100

    Localize page

--- src/routes/[lang=lang]/open-sessies/gratis-open-infosessies/+page.svelte
+++ src/routes/[lang=lang]/open-sessies/gratis-open-infosessies/+page.svelte
@@ -20,14 +20,13 @@
 
       if (error) throw new Error(error.message);
 
-      console.log(data);
+      // console.log(data);
       return data;
    }
-
 </script>
 
 <header>
-   <Hero height="h-92" imgSrc="home/kracht-gezonde-teams.webp" imgAlt="No Alt">
+   <Hero imgSrc="home/kracht-gezonde-teams.webp" imgAlt="No Alt">
       <span slot="title">
          {titleCase($LL.open_sessions.info.title())}
       </span>
@@ -41,40 +40,40 @@
       <Title type="h2" slot="title" large>{$LL.open_sessions.info.intro.title()}</Title>
 
       <P large>
-         {@const introItems = $LL.open_sessions.info.intro.content()}
-         {#each introItems as item}
-            {@html item}
+         {#each Object.entries($LL.open_sessions.info.intro.content) as [key, item]}
+            {@html item()}
          {/each}
       </P>
 
    </SectionWrapper>
 
    <SectionWrapper name="infosessies-praktische-info">
-      <Title type="h2" slot="title">{titleCase($LL.open_sessions.info.practical.title())}</Title>
+      <Title type="h2" slot="title">{titleCase($LL.open_sessions.practical())}</Title>
       <div class="grid grid-cols-2 gap-8">
          {#await getData()}
             <Alert>
                {firstLetterCase($LL.base.db.loading())}...
             </Alert>
          {:then data} 
-         <!-- <pre>{JSON.stringify($pageResult, null, 2)}</pre> -->
             {#each data as session}
                {#if session.type === 'info_session'}
                   <Card label="{formatDateMonthFull(session.starts_on)}" labelPrimary>
-                     <Title slot="title" type="h3 smallest">
+                     <Title slot="title" type="h3" smallest>
                         {titleCase($LL.open_sessions.info.single())}
                      </Title>
                      <div class="flex flex-col gap-4 w-full">
                         <div class="flex flex-row justify-between w-full">
                            <Tag outlined>
+                              <span class="text-xs italic">Trainer:</span><br/>
                               {session.trainer.first_name} {session.trainer.last_name}
                            </Tag>
                            <Tag primary>
+                              <span class="text-xs italic">Tijd:</span><br/>
                               {formatTime(session.starts_on)} - {formatTime(session.ends_on)}
                            </Tag>
                         </div>
                         <Button size="lg" color="primary">
-                           {firstLetterCase($LL.open_sessions.info.btn.subscribe())}
+                           {firstLetterCase($LL.open_sessions.subscribe())}
                         </Button>
                      </div>
                   </Card>
@@ -85,8 +84,15 @@
          {/await}
       </div>
       <div class="m-0 px-0 py-16 w-full flex justify-center items-center">
-         <Button size="xxl">
-            {titleCase($LL.open_sessions.info.btn.subscribe_to_session())}
+         <Button 
+            size="xxl" icon="mdi:pen-plus" 
+            iconClass="
+               h-10 w-10 
+               text-primary group-hover:text-primary-d2
+               mr-2 mt-0.5 px-1 pb-1 pt-[5px]
+               rounded-full bg-gray-50">
+            {titleCase($LL.open_sessions.subscribe_to())}
+            {titleCase($LL.open_sessions.info.single())}
          </Button>
       </div>
    </SectionWrapper>
--- src/routes/[lang=lang]/open-sessies/hybride-lencioni-leertraject/+page.svelte
+++ src/routes/[lang=lang]/open-sessies/hybride-lencioni-leertraject/+page.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
-   import { Main, SectionWrapper, Title, P, Breadcrumbs, List, ListItem, Button } from "$comp/core";
-   import { Hero, Card } from "$comp/common";
-   import { Accordeon, AccordeonItem } from "$comp/content";
-   import Image            from "$src/lib/components/Common/Media/Image.svelte";
    
-   import { formatDateFull, formatDateMonthFull, formatDateShort, formatTime, formatYear, titleCase } from "$utils";
-
+   import { formatDateShort, formatTime, formatYear, titleCase } from "$utils";
+   
    import LL from "$src/i18n/i18n-svelte";
-
-	import Icon from "@iconify/svelte";
-	import PartnershipAccordeon from "../PartnershipAccordeon.svelte";
-	import MasterclassCard from "./MasterclassCard.svelte";
+   
+   import { 
+      Main, SectionWrapper, Title, P, Breadcrumbs, 
+      List, ListItem, Button } from "$comp/core";
+   import { Hero, Card, TwicPic, Modal } from "$comp/common";
+   import { 
+         Accordeon, AccordeonItem, RatingCard, FeaturesSlider, FeaturesSlide, 
+         FeaturesModalContent, Tab, TabList, TabPanel, Tabs 
+      } from "$comp/content";
+   import { PartnershipAccordeon, WorkshopCard, SessionDateCard } from "../open-sessies";
+	
 
    export let data;
    let pageData = Object(data.sessionTypes);
    let sessionData = Object(data.sessionData);
-   console.log(sessionData);
+   let ratingData = Object(data.ratingData);
 
+   import { currentModal } from "$src/lib/stores";
+   $: $currentModal;
+
+   // Set 'active' variable for accordeon component
+	let active:any = null;
+
+   let featureValues;
 </script>
 
 <header>
@@ -29,36 +39,33 @@
    <Breadcrumbs/>
    
    <SectionWrapper name="hybride-lencioni-leertraject">
+
       <P large>
-         {@const introContent = $LL.open_sessions.level_1.intro.content()}
-         {#each introContent as item}
-            {@html item}
+         {#each Object.entries($LL.open_sessions.level_1.intro.content) as [key, item]}
+            {@html item()}
          {/each}
       </P>
       <PartnershipAccordeon />
-   </SectionWrapper>
-
-   <SectionWrapper name="hybride-lencioni-leertraject-features">
-      <div class="flex flex-row gap-4">
-
+      <div class="grid grid-cols-2 gap-4">
+   
          {#each pageData as session}
-            <MasterclassCard {session} />
+            <WorkshopCard {session} class="col-span-1" />
          {/each}
-
+   
       </div>
    </SectionWrapper>
 
    <SectionWrapper name="hybride-lencioni-leertraject-praktische-info">
       <div class="flex flex-row w-full gap-16 items-center justify-start" slot="title">
          <Title type="h2" small>
-            Praktische Info:
+            {titleCase($LL.open_sessions.practical())}:
          </Title>
          <div class="">
             <Title type="subheader" smaller italic>
-               Hybride Lencioni Teamcoaching Leertraject
+               {titleCase($LL.open_sessions.level_1.title_alt())}
             </Title>
             <Title type="h3" smaller thin>
-               Mét Tweedaagse Masterclass
+               {titleCase(`${$LL.open_sessions.level_1.full.title()} ${$LL.open_sessions.level_1.two_day()}`)}
             </Title>
          </div>
       </div>
@@ -66,36 +73,229 @@
       <div class="grid grid-cols-2 md:grid-cols-3 gap-4 xl:gap-8">
          {#each sessionData as session}
             {#if session.type == "level_1_full"}
-               <Card class="
-                     last-of-type:col-span-full md:last-of-type:col-span-1
-                     last-of-type:w-1/2 md:last-of-type:w-full
-                     last-of-type:mx-auto"
-               >
-                  <div slot="title" 
-                     class="flex items-center justify-center w-full h-fit gap-2">
-                     <Icon 
-                        icon="material-symbols:calendar-month-rounded" 
-                        class="w-10 h-10 p-[5px] text-white bg-primary/90 rounded-full" />
-                     <Title type="h4" class="uppercase pt-[5px]" small>
-                        {#if session.is_full}
-                           Volzet
-                        {:else}
-                           <div class="flex flex-col gap-0 leading-none">
-                              {formatDateShort(session.starts_on)} - {formatDateShort(session.ends_on)}<br/>
-                              <span class="text-xl font-medium m-0">{formatYear(session.starts_on)}</span>
-                           </div>
-                        {/if}
-                     </Title>
-                  </div>
-
-                  <P large center class="w-full text-2xl">
-                     {formatTime(session.starts_on)} - {formatTime(session.ends_on)}
-                  </P>
-               </Card>
-
-
+               <SessionDateCard {session} />
             {/if}
          {/each}
       </div>
+      <hr class="border-2 border-primary/70"/>
+      <div class="grid grid-cols-2 md:grid-cols-3 gap-4 xl:gap-8">
+         
+         <Modal icon="ph:student-bold">
+            <Button slot="trigger" block outlined center bold size="lg" icon="ph:student-bold">
+               {titleCase($LL.open_sessions.your_trainer.title())}
+            </Button>
+            
+            <Title slot="title" type="h3" small>{titleCase($LL.open_sessions.your_trainer.title())}</Title>
+            <div class="grid grid-cols-12 gap-4 w-full mx-auto">
+               <div class="col-span-4 items-center justify-center">
+                  <div class="flex flex-col">
+                     <Title type="subheader" italic>{titleCase($LL.open_sessions.your_trainer.tom())}</Title>
+                     <hr/>
+                     <P large>
+                        {@html $LL.open_sessions.your_trainer.content()}
+                     </P>
+                  </div>
+               </div>
+               <div class="col-span-8">
+                  <TwicPic 
+                     alt="Tom van Dorst - Trixolutions CEO / Trainer / Coach"
+                     src="team/tom-bw.webp" 
+                     mode="cover" position="top" 
+                     ratio="3:2" class="w-full h-auto"/>
+                  </div>
+               </div>
+         </Modal>
+
+         <Modal icon='mdi:map-marker-radius'>
+            <Button slot="trigger" block outlined center bold size="lg" icon="mdi:map-marker-radius">
+               {titleCase($LL.open_sessions.location())}
+            </Button>
+            <Title slot="title" type="h3" small>{titleCase($LL.open_sessions.location())}</Title>
+            <div class="relative text-right h-full w-full">
+               <div class="overflow-hidden bg-none h-full w-full">
+                  <iframe 
+                     title="{titleCase($LL.open_sessions.location())} {titleCase($LL.open_sessions.level_1.title_alt())}"
+                     class="w-full min-w-full h-120"
+                     id="gmap_canvas" 
+                     src="https://maps.google.com/maps?q=Domein%20Martinus&t=&z=9&ie=UTF8&iwloc=&output=embed" 
+                     frameborder="0" scrolling="no" 
+                     marginheight="0" marginwidth="0"></iframe>
+               </div>
+            </div>
+         </Modal>
+
+         <Modal icon="mdi:calendar-text">
+            <Button slot="trigger" block outlined center bold size="lg" icon="mdi:calendar-text">
+               Programma
+            </Button>
+            <Title slot="title" type="h3" small>Programma</Title>
+            <Tabs>
+               <TabList>
+                  <Tab>Voortraject</Tab>
+                  <Tab>Dag 1</Tab>
+                  <Tab>Dag 2</Tab>
+                  <Tab>Natraject</Tab>
+               </TabList>
+               <TabPanel>
+                  <List>
+                     <ListItem large>Je ontvangt een schriftelijke intake om je verwachtingen goed in kaart te brengen.</ListItem>
+                     <ListItem large>Je ontvangt een e-mail + link om het Lencioni “Team Assessment” in te vullen.</ListItem>
+                  </List>
+               </TabPanel>
+               <TabPanel>
+                  <List>
+                     <ListItem large><strong>Wie is Patrick Lencioni</strong> en zijn Table Group?</ListItem>
+                     <ListItem large>Lencioni's meest succesvolle boeken, inzichten en verbanden met elkaar.</ListItem>
+                     <ListItem large>Ons Trixolutions lidmaatschap met The Table Group – wat houdt dat juist in?</ListItem>
+                     <ListItem large>Gedachtengoed van de Lencioni Piramide uit <em>“De 5 Frustraties van Teamwork”</em>.</ListItem>
+                     <br/>
+                     <ListItem large>Tussentijdse oefeningen tegen dag 2.</ListItem>
+                  </List>
+                  <hr/>
+                  <div class="grid grid-cols-2 gap-4">
+                     <div class="col-span-1">
+                        <Title type="h5" small>Piramide Laag 1</Title>
+                        <List>
+                           <Title type="subtitle" italic>"Vertrouwen"</Title>
+                           <ListItem large>Welke Lencioni-oefeningen zijn er beschikbaar voor een teamcoach en hoe deze in te zetten?</ListItem>
+                        </List>
+                     </div>
+                     <div class="col-span-1">
+                        <Title type="h5" small>Piramide Laag 2</Title>
+                        <List>
+                           <Title type="subtitle" smallest italic>"Respectvolle Conflicten"</Title>
+                           <ListItem large>Welke Lencioni-oefeningen zijn er beschikbaar voor een teamcoach en hoe deze in te zetten?</ListItem>
+                        </List>
+                     </div>
+                  </div>
+               </TabPanel>
+               <TabPanel></TabPanel>
+               <TabPanel>
+                  <List>
+                     <ListItem large>Je kan met de trainer (Tom) een uur persoonlijke opvolgcoaching inplannen.</ListItem>
+                     <ListItem large>Je krijgt gedurende 12 maanden, elke maand, een Lencioni Newsletter met nieuwe en extra oefeningen, samenvattingen van Lencioni Podcasts en inzichten uit de Lencioni CAPA PRO Academy.   </ListItem>
+                     <ListItem large>Je kan het volledige digitale Lencioni Team Assessment 1 keer inzetten, onder je eigen logo, ter waarde van 490€.</ListItem>
+                     
+                  </List>
+               </TabPanel>
+            </Tabs>
+         </Modal>
+
+         <Modal icon="mdi:account-group-outline">
+            <Button slot="trigger" block outlined center bold size="lg" icon="mdi:account-group-outline">
+               {titleCase($LL.open_sessions.audience())}
+            </Button>
+            <Title slot="title" type="h3" small>{titleCase($LL.open_sessions.audience())}</Title>
+            <P large>
+               {$LL.open_sessions.level_1.audience()}
+            </P>
+         </Modal>
+
+         <!-- Aanpak -->
+         <Modal icon="mdi:tools">
+            <Button slot="trigger" block outlined center bold size="lg" icon="mdi:tools">
+               {titleCase($LL.open_sessions.approach())}
+            </Button>
+            <Title slot="title" type="h3" small>{titleCase($LL.open_sessions.approach())}</Title>
+            <P large italic>
+               {$LL.open_sessions.level_1.approach.intro()}
+            </P><br/>
+            <List>
+               <Title type="h4" small>{titleCase($LL.open_sessions.level_1.approach.results.title())}</Title>
+               {#each Object.entries($LL.open_sessions.level_1.approach.results.items) as [key, item]}
+                  <ListItem large>{item()}</ListItem>
+               {/each}
+            </List>
+         </Modal>
+
+         <!-- Recensies -->
+         <Modal icon="mdi:star">
+            <Button slot="trigger" block outlined center bold size="lg" icon="mdi:star">
+               {titleCase($LL.open_sessions.ratings())}
+            </Button>
+            <Title slot="title" type="h3" small>{titleCase($LL.open_sessions.ratings())}</Title>
+            <div class="columns-3">
+               {#each ratingData as rating}
+                  {#if rating.session_rated === 'level_1_full'}
+                     <RatingCard 
+                        rating={rating.rating}
+                        class="mb-4">
+                        {rating.content.nl}
+                     </RatingCard>
+                  {/if}
+               {/each}
+            </div>
+            <hr class="border-2 border-primary/30 my-4"/>
+            <div class="columns-3">
+               {#each Array(8) as img}
+                  <div class="
+                        overflow-hidden group mb-[5%] rounded-xl 
+                        transition-all duration-300 ease-out
+                        shadow-lg shadow-black/30 
+                        hover:shadow-black/50
+                        h-auto w-full
+                        hover:-translate-y-0.5
+                        cursor-zoom-in
+                        ">
+                     <TwicPic 
+                        alt="No alt"
+                        src="" 
+                        mode="cover" position="top" 
+                        ratio="3:2"
+                        intrinsic="600x400"/>
+                  </div>
+               {/each}
+            </div>
+         </Modal>
+      </div>
    </SectionWrapper>
+
+   <SectionWrapper name="hybride-lencioni-leertraject-features">
+      <Title slot="title" type="h2" small>
+         {$LL.open_sessions.level_1.features_title()}
+      </Title>
+      <FeaturesSlider>
+         {#each Object.entries($LL.open_sessions.level_1.features_extended) as [key, feature]}
+            <FeaturesSlide 
+               modalId={Number(key)+1} 
+               imgSrc="" imgAlt="placeholder" 
+               class="{(Number(key)+1) >= (Array.from($LL.open_sessions.level_1.features_extended).length) ? 'mb-20 hover:mb-16' : 'mb-8 hover:mb-4' }">
+               <span slot="subtitle">{feature.title.before()}</span>
+               <span slot="title">{feature.title.main()}</span>
+            </FeaturesSlide>
+         {/each}
+         
+      </FeaturesSlider>
+
+      <Accordeon bind:active>
+         <AccordeonItem 
+            id={1} 
+            title={$LL.open_sessions.level_1.expand.title()} titleSmaller
+            icon="majesticons:arrows-expand-full">
+            <P large>
+               {@html $LL.open_sessions.level_1.expand.text()}
+            </P>
+         </AccordeonItem>
+      </Accordeon>
+   </SectionWrapper>
+
+   {#each Object.entries($LL.open_sessions.level_1.features_extended) as [key, feature]}
+      <FeaturesModalContent id={Number(key)+1} imgSrc="" imgAlt="placeholder">
+         <span slot="title">
+            {#each Object.entries(feature.title) as [titleKey, title]}
+                  {title()}&nbsp;
+            {/each}
+         </span>
+         <P large>
+            {#each Object.entries(feature.content) as [contentKey, content]}
+                  {@html content()}
+            {/each}
+         </P>
+         <span slot="extra" class:hidden={!feature.extra}>
+               {#if feature.extra}
+                  {feature.extra()}
+               {/if}
+         </span>
+      </FeaturesModalContent>
+   {/each}
 </Main>
