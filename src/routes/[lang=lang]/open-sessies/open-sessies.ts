ea44d809fa08d9c0f365a8ca7169ecc677039b1a
raremiroir <mirostorm@gmail.com>
2023-01-09 01:57:21 +0100

    Add export for custom content components

---
+++ src/routes/[lang=lang]/open-sessies/open-sessies.ts
@@ -0,0 +1,5 @@
+export { default as PartnershipAccordeon }   from './components/PartnershipAccordeon.svelte'
+export { default as WorkshopCard }           from './components/WorkshopCard.svelte'
+export { default as SessionDateCard }        from './components/SessionDateCard.svelte'
+
+export { default as ModalLocation }        from './components/ModalLocation.svelte'
