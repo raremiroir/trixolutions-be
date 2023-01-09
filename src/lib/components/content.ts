aa116b33750bff40705ecf0d69a8712a9e2a7372
raremiroir <mirostorm@gmail.com>
2023-01-09 01:48:25 +0100

    Export new components

--- src/lib/components/content.ts
+++ src/lib/components/content.ts
@@ -3,10 +3,14 @@ export { default as AccordeonItem } from './Content/Accordeon/AccordeonItem.svel
 
 export { default as Menu }          from './Content/Menu.svelte';
 
-export { default as Slider }        from './Content/Slider/Slider.svelte';
-export { default as Slide }         from './Content/Slider/Slide.svelte';
-
 export { default as Tabs }          from './Content/Tabs/Tabs.svelte';
 export { default as Tab }           from './Content/Tabs/Tab.svelte';
 export { default as TabList }       from './Content/Tabs/TabList.svelte';
 export { default as TabPanel }      from './Content/Tabs/TabPanel.svelte';
+
+export { default as RatingCard }    from './Content/Rating/RatingCard.svelte';
+export { default as Rating }        from './Content/Rating/Rating.svelte';
+
+export { default as FeaturesSlider } from './Content/FeaturesSlider/FeaturesSlider.svelte';
+export { default as FeaturesSlide } from './Content/FeaturesSlider/FeaturesSlide.svelte';
+export { default as FeaturesModalContent } from './Content/FeaturesSlider/FeaturesModalContent.svelte';
