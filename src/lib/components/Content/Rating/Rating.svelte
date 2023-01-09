6f748b30ac707063713dcd55ee6344b8106bea5c
raremiroir <mirostorm@gmail.com>
2023-01-09 01:50:10 +0100

    Add rating component

---
+++ src/lib/components/Content/Rating/Rating.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+
+   let klass = '';
+   export { klass as class };
+   export let rating = 3;
+   export let size = 'w-8'
+
+   rating = rating * 2;
+   console.log(rating);
+
+   let halfStar = false
+
+   if (rating % 2 !== 0) {
+      rating = rating - 1;
+      halfStar = true  
+   }
+
+   const amtOfStars = rating / 2;
+   
+</script>
+
+<div class="flex flex-row gap-1 {klass} text-warning">
+   {#if amtOfStars > 0}
+      {#each Array(amtOfStars) as star}
+         <Icon icon="mdi:star" class="{size} h-auto" />
+      {/each}
+      {#if halfStar}
+         <Icon icon="mdi:star-half" class="{size} h-auto" />
+      {/if}
+   {/if}
+</div>
---
+++ src/lib/components/Content/Rating/RatingCard.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Card } from "../../common";
+	import { P } from "../../core";
+   import Rating from "./Rating.svelte";
+
+   export let rating = 3
+
+   let klass = ''
+   export { klass as class }
+   export let size = 'w-7'
+
+</script>
+
+<Card class={klass}>
+   <P italic>
+      “<slot/>”
+   </P>
+   <Rating {rating} {size} />
+</Card>
