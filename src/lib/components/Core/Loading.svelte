e5bcc48c4ecf0fe288dbbece8df7f4cbc808df02
raremiroir <mirostorm@gmail.com>
2023-01-10 03:15:37 +0100

    Create loading component

---
+++ src/lib/components/Core/Loading.svelte
@@ -0,0 +1,485 @@
+<script>
+   // Color can be primary or light
+   export let color = 'primary';
+   
+   export let width = "w-80"
+
+</script>
+
+<div class="
+        w-full h-full 
+        left-0 top-0 right-0 bottom-0 
+        grid place-items-center
+        bg-transparent
+        ">
+   <svg 
+      id="a" data-name="Loading Trixolutions" 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 600.82703 86.89584" 
+      class="{width} h-auto">
+      <path id="b" 
+         data-name="x" 
+         d="M118.936,21.15133h22.354l7.798,13.6992,9.09401-13.6992h20.78099l-16.767,23.4183,17.97,25.7323h-21.98399l-9.09401-15.8281-20.841,30.922h-20.708l28.323-40.8262-16.926-23.4183Z" 
+         style="stroke-miterlimit: 10;" 
+         class="svg-elem-1 fill-[#afb1c6] stroke-[#afb1c6] stroke-[3px] ">
+      </path>
+      <g class="stroke-[3px] {color === 'light' ? 'stroke-gray-50' : 'stroke-primary'} fill-transparent"
+         style="stroke-miterlimit: 1;">
+         <path id="c" 
+            data-name="t" 
+            d="M27.3249,2.45373V21.15133h10.367v13.7918h-10.367v17.4134c0,2.0939,.2002,3.4799,.6017,4.1565,.6167,1.047,1.6963,1.5707,3.2396,1.5707,1.3885,0,3.3323-.4,5.8315-1.2014l1.3884,13.0031c-4.6593,1.0182-9.0097,1.5273-13.0513,1.5273-4.6905,0-8.1455-.6012-10.367-1.803-2.2214-1.2019-3.86468-3.02679-4.92866-5.4768-1.06495-2.45-1.59694-6.4175-1.59694-11.9026v-17.2872H1.5v-13.7918h6.94218V12.1265L27.3249,2.45373Z" 
+            class="svg-elem-2">
+         </path>
+         <path id="d" 
+            data-name="r" 
+            d="M47.8036,21.15133h17.6331v8.0529c1.6964-3.4862,3.4482-5.885,5.2527-7.1964,1.8054-1.3115,4.0338-1.9672,6.6879-1.9672,2.7768,0,5.8153,.8642,9.1174,2.5917l-5.8315,13.4216c-2.2215-.9257-3.9802-1.3885-5.276-1.3885-2.469,0-4.3816,1.0182-5.7389,3.0546-1.9438,2.8694-2.9157,8.238-2.9157,16.1058v16.4761h-18.929V21.15133Z" 
+            class="svg-elem-3">
+         </path>
+         <path id="e" 
+            data-name="i" 
+            d="M91.3298,2.45373h18.8362V15.27363h-18.8362V2.45373Zm0,18.6976h18.8362v49.1506h-18.8362V21.15133Z" 
+            class="svg-elem-4"></path>
+         <path id="f" 
+            data-name="o" 
+            d="M183.381,45.86554c0-7.4976,2.53-13.6759,7.59-18.5359,5.06001-4.859,11.895-7.2891,20.50301-7.2891,9.842,0,17.278,2.8544,22.30701,8.5621,4.04199,4.5979,6.06299,10.2593,6.06299,16.9852,0,7.5599-2.50699,13.7533-7.51999,18.5816-5.015,4.8293-11.94901,7.2433-20.804,7.2433-7.899,0-14.28601-2.0052-19.16-6.0166-5.987-4.9672-8.979-11.4777-8.979-19.5306Zm18.88301-.0463c0,4.3816,.88699,7.6213,2.661,9.719,1.774,2.0987,4.00299,3.1471,6.688,3.1471,2.71399,0,4.944-1.0333,6.687-3.1008,1.743-2.0666,2.61499-5.3837,2.61499-9.9504,0-4.2579-.879-7.4279-2.638-9.5111-1.75899-2.0822-3.93399-3.1237-6.52599-3.1237-2.746,0-5.01401,1.0571-6.80301,3.1705-1.78999,2.1133-2.68399,5.3301-2.68399,9.6494h0Z" 
+            class="svg-elem-5">
+         </path>
+         <path id="g" 
+            data-name="l" 
+            d="M250.511,2.45373h18.88301V70.30193h-18.88301V2.45373Z" 
+            class="svg-elem-6">
+         </path>
+         <path id="h" 
+            data-name="u" 
+            d="M334.16299,70.30193h-17.58701v-7.9603c-2.62299,3.2708-5.26898,5.6-7.93698,6.9884-2.66901,1.3884-5.94601,2.0827-9.83401,2.0827-5.18399,0-9.24899-1.5502-12.19501-4.6515-2.94699-3.1004-4.41998-7.8747-4.41998-14.3238V21.15133h18.92899v27.0282c0,3.0858,.57001,5.2761,1.71201,6.5719,1.14099,1.2959,2.746,1.9439,4.815,1.9439,2.25,0,4.09399-.8633,5.53-2.5918,1.43399-1.7275,2.151-4.8283,2.151-9.3025V21.15133h18.836v49.1506h0Z" 
+            class="svg-elem-7">
+         </path>
+         <path id="i" 
+            data-name="t 2" 
+            d="M369.26801,2.45373V21.15133h10.367v13.7918h-10.367v17.4134c0,2.0939,.19998,3.4799,.60098,4.1565,.61801,1.047,1.69702,1.5707,3.241,1.5707,1.38702,0,3.332-.4,5.83002-1.2014l1.38901,13.0031c-4.659,1.0182-9.009,1.5273-13.052,1.5273-4.689,0-8.14502-.6012-10.367-1.803-2.22101-1.2019-3.86401-3.02679-4.92801-5.4768-1.065-2.45-1.59698-6.4175-1.59698-11.9026v-17.2872h-6.94202v-13.7918h6.94202V12.1265l18.883-9.67277Z" 
+            class="svg-elem-8">
+         </path>
+         <path id="j" 
+            data-name="i 2" 
+            d="M390.255,2.45373h18.83701V15.27363h-18.83701V2.45373Zm0,18.6976h18.83701v49.1506h-18.83701V21.15133Z" 
+            class="svg-elem-9">
+         </path>
+         <path id="k" 
+            data-name="o 2" 
+            d="M419.758,45.86554c0-7.4976,2.53-13.6759,7.59-18.5359,5.06-4.859,11.89401-7.2891,20.50201-7.2891,9.84198,0,17.27798,2.8544,22.30798,8.5621,4.04102,4.5979,6.06302,10.2593,6.06302,16.9852,0,7.5599-2.508,13.7533-7.521,18.5816-5.01501,4.8293-11.948,7.2433-20.80301,7.2433-7.89902,0-14.28601-2.0052-19.16101-6.0166-5.98599-4.9672-8.978-11.4777-8.978-19.5306Zm18.883-.0463c0,4.3816,.88602,7.6213,2.66101,9.719,1.77301,2.0987,4.00299,3.1471,6.68701,3.1471,2.715,0,4.944-1.0333,6.68799-3.1008,1.74301-2.0666,2.61499-5.3837,2.61499-9.9504,0-4.2579-.879-7.4279-2.63898-9.5111-1.75803-2.0822-3.93301-3.1237-6.52502-3.1237-2.74698,0-5.01398,1.0571-6.80399,3.1705-1.789,2.1133-2.68301,5.3301-2.68301,9.6494h0Z" 
+            class="svg-elem-10">
+         </path>
+         <path id="l" 
+            data-name="n" 
+            d="M486.23999,21.15133h17.54102v8.0066c2.62201-3.2698,5.276-5.6073,7.961-7.0118,2.68402-1.4035,5.95398-2.1056,9.81097-2.1056,5.21405,0,9.29504,1.5502,12.242,4.6515,2.94501,3.1004,4.42004,7.8907,4.42004,14.3701v31.2398h-18.92902v-27.0282c0-3.0848-.57202-5.2683-1.71301-6.5485-1.14197-1.2803-2.74701-1.9209-4.81299-1.9209-2.284,0-4.13501,.8642-5.55402,2.5917-1.41901,1.7285-2.129,4.8293-2.129,9.3025v23.6034h-18.83701V21.15133Z" 
+            class="svg-elem-11">
+         </path>
+         <path id="m" 
+            data-name="s" 
+            d="M547.07501,56.64904l18.698-1.7587c.77197,2.2215,1.85101,3.8111,3.23999,4.7669,1.388,.9568,3.23901,1.4348,5.55402,1.4348,2.52997,0,4.48901-.5388,5.87695-1.6179,1.08002-.8004,1.62006-1.8011,1.62006-3.0029,0-1.3554-.71002-2.4033-2.12805-3.1428-1.01898-.5237-3.71899-1.1702-8.09998-1.9409-6.54199-1.14-11.08398-2.1957-13.63-3.1666-2.54498-.9704-4.69-2.6122-6.43298-4.9238-1.74402-2.3116-2.61499-4.9472-2.61499-7.9063,0-3.2368,.94196-6.0258,2.82397-8.3691,1.88098-2.3423,4.47302-4.0913,7.77502-5.2468,3.30096-1.1556,7.729-1.7344,13.28198-1.7344,5.862,0,10.18903,.4477,12.98297,1.3422,2.79102,.8954,5.12103,2.2838,6.98804,4.1653,1.86597,1.8824,3.41699,4.4279,4.651,7.6364l-17.86401,1.7587c-.46301-1.5736-1.23499-2.7306-2.315-3.4711-1.48102-.987-3.27002-1.481-5.36798-1.481-2.12903,0-3.67902,.3775-4.65204,1.1327-.97095,.7546-1.45697,1.6709-1.45697,2.7491,0,1.2018,.61597,2.1104,1.85101,2.7262,1.23297,.6162,3.91797,1.1706,8.05298,1.6641,6.263,.7084,10.922,1.6949,13.97699,2.9591,3.05402,1.2642,5.39203,3.0667,7.01099,5.4091,1.62006,2.3428,2.43005,4.9165,2.43005,7.7211,0,2.8358-.85602,5.5947-2.56805,8.2761-1.71295,2.6813-4.41296,4.8161-8.09998,6.4033-3.68701,1.5867-8.70898,2.3808-15.06403,2.3808-8.97894,0-15.37299-1.2798-19.18396-3.8413-3.81104-2.5606-6.255-6.2017-7.336-10.9224v.0001Z" 
+            class="svg-elem-12">
+         </path>
+      </g>
+    </svg>
+</div>
+
+<style>
+   /***************************************************
+ * Generated by SVG Artista on 1/9/2023, 2:22:00 PM
+ * MIT license (https://opensource.org/licenses/MIT)
+ * W. https://svgartista.net
+ **************************************************/
+
+@-webkit-keyframes animate-svg-stroke-1 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 314.3485107421875px;
+    stroke-dasharray: 314.3485107421875px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 628.697021484375px;
+    stroke-dasharray: 314.3485107421875px;
+  }
+}
+
+@keyframes animate-svg-stroke-1 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 314.3485107421875px;
+    stroke-dasharray: 314.3485107421875px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 628.697021484375px;
+    stroke-dasharray: 314.3485107421875px;
+  }
+}
+
+@-webkit-keyframes animate-svg-fill-1 {
+  0%, 5%, 95%, 100% {
+    fill: transparent;
+  }
+
+  40%, 60% {
+    fill: rgb(175, 177, 198);
+  }
+}
+
+@keyframes animate-svg-fill-1 {
+  0%, 5%, 95%, 100% {
+    fill: transparent;
+  }
+
+  40%, 60% {
+    fill: rgb(175, 177, 198);
+  }
+}
+
+.svg-elem-1 {
+  -webkit-animation: animate-svg-stroke-1 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s both infinite,
+                       animate-svg-fill-1 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s both infinite;
+          animation: animate-svg-stroke-1 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s both infinite,
+               animate-svg-fill-1 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-2 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 218.29080200195312px;
+    stroke-dasharray: 218.29080200195312px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 436.58160400390625px;
+    stroke-dasharray: 218.29080200195312px;
+  }
+}
+
+@keyframes animate-svg-stroke-2 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 218.29080200195312px;
+    stroke-dasharray: 218.29080200195312px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 436.58160400390625px;
+    stroke-dasharray: 218.29080200195312px;
+  }
+}
+
+.svg-elem-2 {
+  -webkit-animation: animate-svg-stroke-2 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.05s both infinite,
+                       animate-svg-fill-2 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.1s both infinite;
+          animation: animate-svg-stroke-2 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.05s both infinite,
+               animate-svg-fill-2 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.1s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-3 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 181.38055419921875px;
+    stroke-dasharray: 181.38055419921875px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 362.7611083984375px;
+    stroke-dasharray: 181.38055419921875px;
+  }
+}
+
+@keyframes animate-svg-stroke-3 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 181.38055419921875px;
+    stroke-dasharray: 181.38055419921875px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 362.7611083984375px;
+    stroke-dasharray: 181.38055419921875px;
+  }
+}
+
+.svg-elem-3 {
+  -webkit-animation: animate-svg-stroke-3 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.1s both infinite,
+                       animate-svg-fill-3 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.2s both infinite;
+          animation: animate-svg-stroke-3 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.1s both infinite,
+               animate-svg-fill-3 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.2s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-4 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 201.2857666015625px;
+    stroke-dasharray: 201.2857666015625px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 402.571533203125px;
+    stroke-dasharray: 201.2857666015625px;
+  }
+}
+
+@keyframes animate-svg-stroke-4 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 201.2857666015625px;
+    stroke-dasharray: 201.2857666015625px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 402.571533203125px;
+    stroke-dasharray: 201.2857666015625px;
+  }
+}
+
+.svg-elem-4 {
+  -webkit-animation: animate-svg-stroke-4 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.15000000000000002s both infinite,
+                       animate-svg-fill-4 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.30000000000000004s both infinite;
+          animation: animate-svg-stroke-4 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.15000000000000002s both infinite,
+               animate-svg-fill-4 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.30000000000000004s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-5 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 245.63961791992188px;
+    stroke-dasharray: 245.63961791992188px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 491.27923583984375px;
+    stroke-dasharray: 245.63961791992188px;
+  }
+}
+
+@keyframes animate-svg-stroke-5 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 245.63961791992188px;
+    stroke-dasharray: 245.63961791992188px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 491.27923583984375px;
+    stroke-dasharray: 245.63961791992188px;
+  }
+}
+
+.svg-elem-5 {
+  -webkit-animation: animate-svg-stroke-5 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.2s both infinite,
+                       animate-svg-fill-5 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.4s both infinite;
+          animation: animate-svg-stroke-5 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.2s both infinite,
+               animate-svg-fill-5 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.4s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-6 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 175.46243286132812px;
+    stroke-dasharray: 175.46243286132812px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 350.92486572265625px;
+    stroke-dasharray: 175.46243286132812px;
+  }
+}
+
+@keyframes animate-svg-stroke-6 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 175.46243286132812px;
+    stroke-dasharray: 175.46243286132812px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 350.92486572265625px;
+    stroke-dasharray: 175.46243286132812px;
+  }
+}
+
+.svg-elem-6 {
+  -webkit-animation: animate-svg-stroke-6 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.25s both infinite,
+                       animate-svg-fill-6 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 2s both infinite;
+          animation: animate-svg-stroke-6 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.25s both infinite,
+               animate-svg-fill-6 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 2s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-7 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 274.35302734375px;
+    stroke-dasharray: 274.35302734375px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 548.7060546875px;
+    stroke-dasharray: 274.35302734375px;
+  }
+}
+
+@keyframes animate-svg-stroke-7 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 274.35302734375px;
+    stroke-dasharray: 274.35302734375px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 548.7060546875px;
+    stroke-dasharray: 274.35302734375px;
+  }
+}
+
+.svg-elem-7 {
+  -webkit-animation: animate-svg-stroke-7 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.30000000000000004s both infinite,
+                       animate-svg-fill-7 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.6000000000000001s both infinite;
+          animation: animate-svg-stroke-7 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.30000000000000004s both infinite,
+               animate-svg-fill-7 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.6000000000000001s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-8 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 218.29054260253906px;
+    stroke-dasharray: 218.29054260253906px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 436.5810852050781px;
+    stroke-dasharray: 218.29054260253906px;
+  }
+}
+
+@keyframes animate-svg-stroke-8 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 218.29054260253906px;
+    stroke-dasharray: 218.29054260253906px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 436.5810852050781px;
+    stroke-dasharray: 218.29054260253906px;
+  }
+}
+
+.svg-elem-8 {
+  -webkit-animation: animate-svg-stroke-8 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.35000000000000003s both infinite,
+                       animate-svg-fill-8 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.7000000000000001s both infinite;
+          animation: animate-svg-stroke-8 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.35000000000000003s both infinite,
+               animate-svg-fill-8 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.7000000000000001s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-9 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 201.28903198242188px;
+    stroke-dasharray: 201.28903198242188px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 402.57806396484375px;
+    stroke-dasharray: 201.28903198242188px;
+  }
+}
+
+@keyframes animate-svg-stroke-9 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 201.28903198242188px;
+    stroke-dasharray: 201.28903198242188px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 402.57806396484375px;
+    stroke-dasharray: 201.28903198242188px;
+  }
+}
+
+.svg-elem-9 {
+  -webkit-animation: animate-svg-stroke-9 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.4s both infinite,
+                       animate-svg-fill-9 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.8s both infinite;
+          animation: animate-svg-stroke-9 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.4s both infinite,
+               animate-svg-fill-9 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.8s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-10 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 245.6395721435547px;
+    stroke-dasharray: 245.6395721435547px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 491.2791442871094px;
+    stroke-dasharray: 245.6395721435547px;
+  }
+}
+
+@keyframes animate-svg-stroke-10 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 245.6395721435547px;
+    stroke-dasharray: 245.6395721435547px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 491.2791442871094px;
+    stroke-dasharray: 245.6395721435547px;
+  }
+}
+
+.svg-elem-10 {
+  -webkit-animation: animate-svg-stroke-10 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.45s both infinite,
+                       animate-svg-fill-10 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.9s both infinite;
+          animation: animate-svg-stroke-10 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.45s both infinite,
+               animate-svg-fill-10 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.9s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-11 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 274.34991455078125px;
+    stroke-dasharray: 274.34991455078125px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 548.6998291015625px;
+    stroke-dasharray: 274.34991455078125px;
+  }
+}
+
+@keyframes animate-svg-stroke-11 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 274.34991455078125px;
+    stroke-dasharray: 274.34991455078125px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 548.6998291015625px;
+    stroke-dasharray: 274.34991455078125px;
+  }
+}
+
+.svg-elem-11 {
+  -webkit-animation: animate-svg-stroke-11 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.7s both infinite,
+                       animate-svg-fill-11 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.7s both infinite;
+          animation: animate-svg-stroke-11 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.7s both infinite,
+               animate-svg-fill-11 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.7s both infinite;
+}
+
+@-webkit-keyframes animate-svg-stroke-12 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 294.7398681640625px;
+    stroke-dasharray: 294.7398681640625px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 589.479736328125px;
+    stroke-dasharray: 294.7398681640625px;
+  }
+}
+
+@keyframes animate-svg-stroke-12 {
+  0%, 5%, 95%, 100% {
+    stroke-dashoffset: 294.7398681640625px;
+    stroke-dasharray: 294.7398681640625px;
+  }
+
+  40%, 60% {
+    stroke-dashoffset: 589.479736328125px;
+    stroke-dasharray: 294.7398681640625px;
+  }
+}
+
+.svg-elem-12 {
+  -webkit-animation: animate-svg-stroke-12 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.55s both infinite,
+                       animate-svg-fill-12 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.5s both infinite;
+          animation: animate-svg-stroke-12 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.1s both infinite,
+               animate-svg-fill-12 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) 1.1s both infinite;
+}
+
+</style>
