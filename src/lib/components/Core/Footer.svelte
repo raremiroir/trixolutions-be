34b9609054d7a6f614659b31b3942caace611fd7
raremiroir <mirostorm@gmail.com>
2023-01-10 03:15:22 +0100

    Create responsive columns

--- src/lib/components/Core/Footer.svelte
+++ src/lib/components/Core/Footer.svelte
@@ -9,13 +9,13 @@
 <footer class="bg-gray-700 w-full">
 
    <!-- Columns wrap -->
-   <div class="grid grid-cols-3 gap-16 w-3/4 mx-auto pt-12 pb-16">
+   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 w-3/4 mx-auto pt-12 pb-16">
       <!-- Column 1 -->
       <div class="flex flex-col gap-4 w-full">
          <div class="">
             <Logo color='fill-gray-50' xcolor='fill-white' width={250}/>
          </div>
-         <div class="grid grid-cols-2 w-full gap-2">
+         <div class="grid grid-cols-2 w-full gap-2 md:gap-4 lg:gap-6 2xl:gap-8">
             <div class="w-full flex flex-col gap-2">
                <h6 class="w-full border-b-2 border-gray-50">Vestiging België</h6>
                <span>
@@ -48,6 +48,8 @@
    </div>
    <div class="px-1 pb-2 text-sm font-semibold tracking-wide text-center w-full">
       <hr class="border-gray-400 mb-2"/>
-      Trixolutions.be - Ontworpen, ontwikkeld en beheerd door Mist Media
+      <Link underline href="https://mistmedia.be" target="_blank">
+         Trixolutions.be - Ontworpen, ontwikkeld en beheerd door Mist Media
+      </Link>
    </div>
 </footer>
