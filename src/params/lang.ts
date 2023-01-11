b296e2e190c31205cb431eed46a1f53dbe21f159
raremiroir <mirostorm@gmail.com>
2023-01-11 19:38:47 +0100

    Add lang parameter

--- src/params/lang.ts
+++ src/params/lang.ts
@@ -3,5 +3,5 @@ import { isLocale } from '$i18n/i18n-util'
 
 // only accept valid languages as a segment in the URL
 export const match: ParamMatcher = (param) => {
-	return isLocale(param)
+	return isLocale(param);
 }
