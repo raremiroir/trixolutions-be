c19353a424725560ea637fb5d47002427a890437
raremiroir <mirostorm@gmail.com>
2023-01-11 19:40:15 +0100

    Add unicode entity file

---
+++ src/lib/constants/entities.ts
@@ -0,0 +1,12 @@
+export const COPYRIGHT_ENTITY = '\u00a9';                // ©
+export const H_ELLIPSIS_ENTITY = '\u2026';               // …
+export const VERTICAL_LINE_ENTITY = '\u007c';            // |
+
+export const HVY_SINGLE_QUOTATION_OPEN = '\u275B';       // ❛
+export const HVY_SINGLE_QUOTATION_CLOSE = '\u275C';      // ❜
+
+export const HVY_DBL_QUOTATION_OPEN = '\u275D';          // ❝
+export const HVY_DBL_QUOTATION_CLOSE = '\u275E';         // ❞
+
+export const DBL_ANGLE_QUOTATION_OPEN = '\u00AB';        // «
+export const DBL_ANGLE_QUOTATION_CLOSE = '\u00BB';       // »
