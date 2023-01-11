d0abc626722c02a8c94ee32fcf1216399ec8cf1d
raremiroir <mirostorm@gmail.com>
2023-01-11 19:41:19 +0100

    Remove console.logs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -30,10 +30,9 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.locale = locale
 	event.locals.LL = LL
 
-	console.info(LL.log({ fileName: 'hooks.server.ts' }))
 
 	// replace html lang attribute with correct language
-	return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', lang) })
+	return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', locale) })
 }
 
 const getPreferredLocale = ({ request }: RequestEvent) => {
