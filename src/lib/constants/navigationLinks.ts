345d7ac64d5633bf22d03876a707840f4f807e39
raremiroir <mirostorm@gmail.com>
2023-01-24 00:07:08 +0100

    Add navigation links constants file

---
+++ src/lib/constants/navigationLinks.ts
@@ -0,0 +1,52 @@
+export const links = {
+   about: {
+      link: {
+         nl: '/over-ons',
+         en: '/about-us',
+         fr: '/a-propos'
+      }
+   },
+   references: {
+      link: {
+         nl: '/referenties',
+         en: '/references',
+         fr: '/references'
+      }
+   },
+   blog: {
+      link: {
+         nl: '/blog',
+         en: '/blog',
+         fr: '/blog'
+      }
+   },
+   contact: {
+      link: {
+         nl: '/contact',
+         en: '/contact',
+         fr: '/contact',
+      }
+   },
+   open_sessions: {
+      link: {
+         nl: "/open-sessies",
+         en: "/open-sessions",
+         fr: "/sessions-ouvertes",
+      },
+      info_sessions: { link: {
+         nl: "/gratis-open-infosessies",
+         en: "/free-open-info-sessions",
+         fr: "/sessions-infos-ouvertes-gratuites",
+      } },
+      hybrid_track: { link: { 
+         nl: "/hybride-lencioni-leertraject",
+         en: "/hybrid-lencioni-learning-track",
+         fr: "/trajet-hybride-lencioni",
+      } },
+      deepdive: { link: {
+         nl: "/lencioni-deepdive-level-2",
+         en: "/lencioni-deepdive-level-2",
+         fr: "/lencioni-deepdive-niveau-2",
+      } },
+   }
+}
