b00687cdbb0bc23d2a0cbfc8f2ae2235af9a257b
raremiroir <mirostorm@gmail.com>
2023-01-26 01:46:01 +0100

    Add current page slug map in stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,24 @@
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 
 export let currentModal = writable();
+
+
+// Define current page slugs in each locale
+export let currentPageMap = writable([
+   {locale: 'en', slug: '/'},
+   {locale: 'fr', slug: '/'},
+   {locale: 'nl', slug: '/'}
+])
+// Export current page map as derived store
+export const langSelection = (() => {
+   const _selectedLocale = writable('en')
+   const _pageLangMap = derived(
+      [currentPageMap, _selectedLocale], 
+      ([$currentPageMap, $_selectedLocale]) => $currentPageMap.find(a => a.locale == $_selectedLocale)
+      )
+
+   return {
+      selectLocale: _selectedLocale.set,
+      ..._pageLangMap
+   }
+})()
