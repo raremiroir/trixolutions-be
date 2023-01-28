9ae4d665dc13bc135eafa3d890c16024bb2f0053
raremiroir <mirostorm@gmail.com>
2023-01-28 03:06:18 +0100

    Rewrite page

--- src/routes/[...lang=lang]/[open_sessions=open_sessions]/[hybrid_track=hybrid_track]/+page.svelte
+++ src/routes/[...lang=lang]/[open_sessions=open_sessions]/[hybrid_track=hybrid_track]/+page.svelte
@@ -4,15 +4,17 @@
    import { 
       Main, Section, Breadcrumbs, SEO,
       H2, H3, H4, Subheader, 
-      Text,  List, ListItem, Button,
-      Hero, Modal, Accordeon, AccordeonItem, 
-      NavSlider, FeatureSlide, FeaturesModalContent, ThumbnailSlide 
+      Text,  List, ListItem, 
+      Hero, Accordeon, AccordeonItem, Reveal, 
    } from "$comp";
    import { 
       PartnershipAccordeon, WorkshopCard, 
       SessionDateCard, ModalLocation, ModalTrainerTom
    } from "../open-sessies";
-	import { SplideTrack } from "@splidejs/svelte-splide";
+	import ModalRatings from "../components/ModalRatings.svelte";
+	import ModalTargetAudience from "../components/ModalTargetAudience.svelte";
+	import ModalApproach from "../components/ModalApproach.svelte";
+	import HybridTrackFeaturesSlider from "./HybridTrackFeaturesSlider.svelte";
    
    // TODO: Replace these
 	import ModalProgramPlacehold from "../components/ModalProgramPlacehold.svelte";
@@ -21,12 +23,9 @@
    import LL, { locale } from "$i18n/i18n-svelte";
    // Import globals
    import { website } from "$lib/config/website";
-   import { titleCase, formatUrl } from "$utils";
+   import { titleCase, formatUrl, formatDateFull } from "$utils";
    import { currentModal } from "$src/lib/stores";
-	import ModalRatings from "../components/ModalRatings.svelte";
-	import ModalTargetAudience from "../components/ModalTargetAudience.svelte";
-	import ModalApproach from "../components/ModalApproach.svelte";
-	import HybridTrackFeaturesSlider from "./HybridTrackFeaturesSlider.svelte";
+   
    $: $currentModal;
 
    // Import data
@@ -34,6 +33,11 @@
    let pageData = Object(data.sessionTypes);
    let sessionData = Object(data.sessionData);
    let ratingData = Object(data.ratingData);
+
+   let sessionDates:any = [];
+   sessionData.forEach((item:any) => {
+      if(item.type === 'level_1_full') sessionDates.push(formatDateFull(item.starts_on));
+   });
 
    // Define current page slug
    import { currentPageMap } from "$lib/stores";
@@ -84,92 +88,112 @@ let pageSlug = formatUrl($LL.sessions.level_1.title_short());
    
    <!-- Intro -->
    <Section name="hybride-lencioni-leertraject" >
-      <Text>
-         {@html $LL.sessions.level_1.intro.content()}
-      </Text>
+      <Reveal>
+         <Text>
+            {@html $LL.sessions.level_1.intro.content()}
+         </Text>
+      </Reveal>
 
-      <PartnershipAccordeon />
+      <Reveal lazy>
+         <PartnershipAccordeon />
+      </Reveal>
 
       <!-- Workshop Cards -->
-      <div class="grid grid-cols-2 gap-4">
+      <Reveal bottom>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-2 lg:gap-4">
          {#each pageData as session}
-            <WorkshopCard {session} class="col-span-1" />
+            <WorkshopCard {session} {sessionDates} class="col-span-1" />
          {/each}
       </div>
+      </Reveal>
    </Section>
 
    <!-- Practical Info -->
    <Section name="hybride-lencioni-leertraject-praktische-info">
       <!-- Title -->
       <div class="flex flex-row w-full gap-16 items-center justify-start" slot="title">
-         <H2 small>
-            {titleCase($LL.sessions.practical())}:
-         </H2>
-         <div class="">
+         <Reveal left>
+            <H2 small>
+               {titleCase($LL.sessions.practical())}:
+            </H2>
+         </Reveal>
+         <Reveal right>
             <Subheader smaller italic>
                {titleCase($LL.sessions.level_1.title_alt())}
             </Subheader>
             <H3 smaller thin>
                {titleCase(`${$LL.sessions.level_1.full.title()} ${$LL.sessions.level_1.two_day()}`)}
             </H3>
-         </div>
+         </Reveal>
       </div>
       <!-- Dates -->
-      <div class="grid grid-cols-2 md:grid-cols-3 gap-4 xl:gap-8">
-         {#each sessionData as session}
-            {#if session.type == "level_1_full"}
-               <SessionDateCard {session} />
-            {/if}
-         {/each}
-      </div>
-      <hr class="border-2 border-primary/70"/>
+      <Reveal lazy>
+         <div class="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-4 xl:gap-8 w-full">
+            {#each sessionData as session}
+               {#if session.type == "level_1_full"}
+                  <SessionDateCard {session} />
+               {/if}
+            {/each}
+         </div>
+      </Reveal>
+      <Reveal lazy>
+         <hr class="border-2 border-primary/70"/>
+      </Reveal>
       
       <!-- Modals -->
-      <div class="grid grid-cols-2 md:grid-cols-3 gap-4 xl:gap-8">
-         <!-- Trainer -->
-         <ModalTrainerTom/>
-         <!-- Location -->
-         <ModalLocation/>
-         <!-- Program -->
-         <ModalProgramPlacehold/>
-         <!-- Target Audience -->
-         <ModalTargetAudience>
-            <Text>{$LL.sessions.level_1.audience()}</Text>
-         </ModalTargetAudience>
-         <!-- Approach -->
-         <ModalApproach>
-            <Text italic>{$LL.sessions.level_1.approach.intro()}</Text><br/>
-            <List>
-               <H4 small>{titleCase($LL.sessions.level_1.approach.results.title())}</H4>
-               {#each Object.entries($LL.sessions.level_1.approach.results.items) as [key, item]}
-                  <ListItem>{item()}</ListItem>
-               {/each}
-            </List>
-         </ModalApproach>
-         <!-- Ratings -->
-         <ModalRatings {ratingData}/>
-      </div>
+      <Reveal bottom lazier>
+         <div class="grid grid-cols-2 md:grid-cols-3 gap-4 xl:gap-8">
+            <!-- Trainer -->
+            <ModalTrainerTom/>
+            <!-- Location -->
+            <ModalLocation/>
+            <!-- Program -->
+            <ModalProgramPlacehold/>
+            <!-- Target Audience -->
+            <ModalTargetAudience>
+               <Text>{$LL.sessions.level_1.audience()}</Text>
+            </ModalTargetAudience>
+            <!-- Approach -->
+            <ModalApproach>
+               <Text italic>{$LL.sessions.level_1.approach.intro()}</Text><br/>
+               <List>
+                  <H4 small>{titleCase($LL.sessions.level_1.approach.results.title())}</H4>
+                  {#each Object.entries($LL.sessions.level_1.approach.results.items) as [key, item]}
+                     <ListItem>{item()}</ListItem>
+                  {/each}
+               </List>
+            </ModalApproach>
+            <!-- Ratings -->
+            <ModalRatings {ratingData}/>
+         </div>
+      </Reveal>
    </Section>
 
    <!-- Features -->
    <Section name="hybride-lencioni-leertraject-features">
-      <H2 slot="title" small>
-         {$LL.sessions.level_1.features_title()}
-      </H2>
+      <Reveal slot='title'>
+         <H2 small>
+            {$LL.sessions.level_1.features_title()}
+         </H2>
+      </Reveal>
 
       <!-- Features Slider -->
-      <HybridTrackFeaturesSlider />
+      <Reveal lazy>
+         <HybridTrackFeaturesSlider />
+      </Reveal>
 
       <!-- Expand Trajectory Accordeon -->
-      <Accordeon bind:active>
-         <AccordeonItem 
-            id={1} 
-            title={$LL.sessions.level_1.expand.title()} titleSmaller
-            icon="majesticons:arrows-expand-full">
-            <Text>
-               {@html $LL.sessions.level_1.expand.text()}
-            </Text>
-         </AccordeonItem>
-      </Accordeon>
+      <Reveal slow>
+         <Accordeon bind:active>
+            <AccordeonItem 
+               id={1} 
+               title={$LL.sessions.level_1.expand.title()} titleSmaller
+               icon="majesticons:arrows-expand-full">
+               <Text>
+                  {@html $LL.sessions.level_1.expand.text()}
+               </Text>
+            </AccordeonItem>
+         </Accordeon>
+      </Reveal>
    </Section>
 </Main>
