9b723a60bdd67ff26ac28d4ed6f538ef5d0f8a86
raremiroir <mirostorm@gmail.com>
2023-01-29 00:53:44 +0100

    Fix images import SEO

--- src/routes/[...lang=lang]/(main)/contact/+page.svelte
+++ src/routes/[...lang=lang]/(main)/contact/+page.svelte
@@ -16,9 +16,9 @@
 	import Icon from "@iconify/svelte";
    
    // Import features image (SEO)
-   import ogSquareImageSrc from '$lib/assets/images/home/home-open-graph-square.jpg';
-	import ogImageSrc from '$lib/assets/images/home/home-open-graph.jpg';
-	import featuredImageSrc from '$lib/assets/images/home/home.jpg';
+   import ogSquareImageSrc from '$lib/assets/images/home/home-open-graph-square.png';
+	import ogImageSrc from '$lib/assets/images/home/home-open-graph.png';
+	import featuredImageSrc from '$lib/assets/images/home/home.png';
    
    // Import i18n
    import LL from '$i18n/i18n-svelte'
