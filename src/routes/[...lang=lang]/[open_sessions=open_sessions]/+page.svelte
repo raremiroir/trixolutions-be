ace69d4fed99f1a30a591ad9303845f8da46aa8e
raremiroir <mirostorm@gmail.com>
2023-01-28 19:31:27 +0100

    Localize

--- src/routes/[...lang=lang]/[open_sessions=open_sessions]/+page.svelte
+++ src/routes/[...lang=lang]/[open_sessions=open_sessions]/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
    // Import components
-   import { Main, Section, Button, Breadcrumbs, Tag, Image, H1, H2, Text, Card } from "$comp";
+   import { Main, Section, Breadcrumbs, H1 } from "$comp";
 	import SessionTypeCard from "./SessionTypeCard.svelte";
    
    // Import i18n
-   import LL from "$i18n/i18n-svelte";
+   import LL, { locale } from "$i18n/i18n-svelte";
    
    // Import utils
 	import { titleCase } from "$src/lib/utils";
@@ -17,29 +17,29 @@
       {locale: 'nl', slug: 'open-sessies'}
    ];
    
-   let session_types = [
+   $: session_types = [
       {
          title: titleCase($LL.sessions.info.title()),
          badge: titleCase($LL.sessions.info.title_short()),
-         link: '/',
-         text: 'Een gratis, open infosessie over onze veelzijdige Lencioni Teamcoaching Opleiding.<br/> Aarzel niet en schrijf je in!',
-         img: 'placeholder/placeholder.png'
+         link: `/${$locale}/${$LL.nav.open_sessions.items.info_sessions.slug()}`,
+         text: $LL.sessions.info.excerpt(),
+         img: 'home/lencioni-ctc.webp'
       },
       {
          title: titleCase($LL.sessions.level_1.title_short()),
          badge: titleCase($LL.sessions.level_1.tag()),
          second_badge: titleCase($LL.sessions.level_1.traject_shorter()),
-         link: '/',
-         text: 'Jouw <strong>hybride Lencioni Leertraject</strong> om je, via online en fysieke leermomenten, te verdiepen en bekwamen als <em>Lencioni Teamcoach</em>, is klaar.',
-         img: 'placeholder/placeholder.png'
+         link: `/${$locale}/${$LL.nav.open_sessions.items.hybrid_traject.slug()}`,
+         text: $LL.sessions.level_1.excerpt(),
+         img: 'home/working-genius.webp'
       },
       {
          title: titleCase($LL.sessions.level_2.title()),
          badge: titleCase($LL.sessions.level_2.advanced_coaching()),
          second_badge: titleCase($LL.sessions.level_2.title()),
-         link: '/',
-         text: `In 1 dag gaan we dieper in op de 4 andere succesvolle modellen van Patrick Lencioni en The Table Group.`,
-         img: 'placeholder/placeholder.png'
+         link: `/${$locale}/${$LL.nav.open_sessions.items.deepdive.slug()}`,
+         text: $LL.sessions.level_2.excerpt(),
+         img: 'home/working-genius.webp'
       },
    ]
 
@@ -60,9 +60,7 @@
                href={session.link}
                img={session.img}
                imgPos={key === 1 ? 'resp-r' : 'resp-l'}
-               ariaLabel={session.title}
-               btn={{title: 'Schrijf me in!', href: '', ariaLabel: 'inschrijven in sessie'}}
-               second_btn={{title: 'Meer info', href: '', ariaLabel: 'inschrijven in sessie'}}
+               btn={{title: 'Meer Info', href: session.link, ariaLabel: $LL.base.btn.more_info()}}
             >
                {@html session.text}
             </SessionTypeCard>
