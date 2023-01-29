250e9c2d71d8cf2291feaeb910dcf854c96f77b9
raremiroir <mirostorm@gmail.com>
2023-01-29 22:45:13 +0100

    Add ordered prop for <ol>-element

--- src/lib/components/Core/List/List.svelte
+++ src/lib/components/Core/List/List.svelte
@@ -3,13 +3,24 @@
    let klass = ''
    export { klass as class };
    export let title = false;
+
+   // Create ordered list
+   export let ordered = false;
+
+   const listClass= 'font-lg font-body'
 </script>
 
 <div class="{klass} w-full">
    {#if title}
       <slot name="title"/>
    {/if}
-   <ul class="font-lg font-body">
-      <slot/>
-   </ul>
+   {#if ordered}
+      <ol class="{listClass} !list-decimal prose-li:!list-decimal">
+         <slot/>
+      </ol>
+   {:else}
+      <ul class={listClass}>
+         <slot/>
+      </ul>
+   {/if}
 </div>
