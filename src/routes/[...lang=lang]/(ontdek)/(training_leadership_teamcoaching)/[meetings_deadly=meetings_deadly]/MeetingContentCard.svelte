3db0635556312071db1af7ebfff40f441d568e33
raremiroir <mirostorm@gmail.com>
2023-01-29 15:32:18 +0100

    Remove hardcoded elements and add localization

--- src/routes/[...lang=lang]/(ontdek)/(training_leadership_teamcoaching)/[meetings_deadly=meetings_deadly]/MeetingContentCard.svelte
+++ src/routes/[...lang=lang]/(ontdek)/(training_leadership_teamcoaching)/[meetings_deadly=meetings_deadly]/MeetingContentCard.svelte
@@ -1,38 +1,61 @@
-<script>
-    import { P, List } from "$comp";
+<script lang="ts">
+    // Import components
+    import { Text, List, ListItem } from "$comp";
 	import ContentCard from "../../ContentCard.svelte";
+
+    // Import i18n
+    import LL from "$i18n/i18n-svelte";
+
+    export let listItems:any = [];
+
+    export let timespan:string;
+    export let target:string;
+    export let target_desc:string;
+
+    $: cardTypes = [
+        {
+            icon: 'material-symbols:nest-clock-farsight-analog-outline-rounded',
+            title: $LL.pages.main.training_leadership_teamcoaching.meetings_deadly.in_depth.key_titles.timespan(),
+            content: 'timespan'
+        },
+        {
+            icon: 'mingcute:target-line',
+            title: $LL.pages.main.training_leadership_teamcoaching.meetings_deadly.in_depth.key_titles.target(),
+            content: 'target'
+        },
+        {
+            icon: 'material-symbols:key-outline-rounded',
+            title: $LL.pages.main.training_leadership_teamcoaching.meetings_deadly.in_depth.key_titles.keys_success(),
+            content: 'list',
+        },
+    ]
 </script>
 
-<P large>
+<Text>
    <slot/>
-</P>
+</Text>
 <div class="grid grid-cols-3 gap-4 xl:gap-8 mt-4">
-   <ContentCard 
-        icon="material-symbols:nest-clock-farsight-analog-outline-rounded"
-        title="Tijdsduur"
-        class="
-            bg-gray-50/50 w-full flex justify-center 
-            col-span-3 sm:col-span-1 lg:col-span-1">
-       <b><slot name="timespan"/></b>
-   </ContentCard>
-   <ContentCard 
-        icon="mingcute:target-line"
-        title="Doel"
-        class="
-            bg-gray-50/50 w-full flex justify-center 
-            col-span-3 sm:col-span-2 lg:col-span-1">
-      <slot name="target"/>
-   </ContentCard>
-   <ContentCard 
-        icon="material-symbols:key-outline-rounded"
-        title="Sleutels tot Succes"
-        class="
-            bg-gray-50/50 w-full flex justify-center 
-            col-span-3 sm:col-span-3 lg:col-span-1">
-       <List>
-           <slot name="key-1"/>
-           <slot name="key-2"/>
-           <slot name="key-3"/>
-       </List>
-   </ContentCard>
+    
+    {#each cardTypes as card, key}
+        <div class="
+                col-span-3
+                { card.content === 'timespan' ? `sm:col-span-1` : card.content === 'target'   ? `sm:col-span-2` : card.content === 'list'     ? `sm:col-span-3` : ``}
+                lg:col-span-1
+                ">
+            <ContentCard height="equal" icon={card.icon} title={card.title}>
+                {#if card.content === 'list'}
+                    <List>
+                        {#each listItems as item}
+                            <ListItem>{item}</ListItem>
+                        {/each}
+                    </List>
+                {:else if card.content === 'timespan'}
+                    <b>{timespan}</b>
+                {:else if card.content === 'target'}
+                    <b>{@html target}</b> <br/>
+                    {@html target_desc}
+                {/if}
+            </ContentCard>
+        </div>
+    {/each}
 </div>
