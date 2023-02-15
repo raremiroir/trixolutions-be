7b1861f952f08d391f3fc7a944b64e8df3d80db9
raremiroir <mirostorm@gmail.com>
2023-02-15 17:49:35 +0100

    Edit spacing

--- src/lib/components/Common/Wrapper/Section.svelte
+++ src/lib/components/Common/Wrapper/Section.svelte
@@ -12,7 +12,7 @@
 <section
    id="{name}"
    class="
-      w-full md:w-[95%] lg:w-[90%] xl:w-4/5 2xl:w-2/3 mx-auto
+      w-full md:w-[95%] lg:w-[90%] xl:w-5/6 2xl:w-3/4 3xl:w-2/3 mx-auto
       flex flex-col gap-8 
       pb-16 pt-8 
       px-1 xs:px-2 sm:px-4 md:px-0
