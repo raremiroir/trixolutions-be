0f4f9ac3002f058863bf63516822355f4bb92f7d
raremiroir <mirostorm@gmail.com>
2023-02-15 16:56:47 +0100

    Update img imports & content styling

--- src/routes/[...lang=lang]/[open_sessions=open_sessions]/[hybrid_track=hybrid_track]/HybridTrackFeaturesSlider.svelte
+++ src/routes/[...lang=lang]/[open_sessions=open_sessions]/[hybrid_track=hybrid_track]/HybridTrackFeaturesSlider.svelte
@@ -7,29 +7,30 @@
 
    // Define features images
    const featureImages = [
-      'lencioni-assets/features/1-inschrijving-hybride-tool.png',
-      'lencioni-assets/features/2-integratie-deelnemers.png',
-      'ratings/ctc-training-1-2.jpeg',
-      'ratings/ctc-groep-be-sept-22.webp',
-      'lencioni-assets/features/5-kartonnen-lencioni-piramide.webp',
-      'lencioni-assets/features/6-team-assessment.png',
-      'lencioni-assets/features/7-personal-assessment.png',
-      'lencioni-assets/features/8-lencioni-werkboek.png',
-      'lencioni-assets/features/9-bijlages-na-tweedaagse.png',
-      'lencioni-assets/features/10-opvolgcoaching.webp',
-      'lencioni-assets/features/11-deepdives.png',
-      'lencioni-assets/features/12-vertrouwen.jpg',
-      'lencioni-assets/features/13-respectvolle-conflicten.png',
-      'lencioni-assets/features/14-team-betrokkenheid.png',
-      'lencioni-assets/features/15-aansprakelijkheid.png',
-      'lencioni-assets/features/16-resultaatgerichtheid.png',
-      'lencioni-assets/features/17-bruggen-andere-modellen.webp',
+      'lencioni/features/1-inschrijving-hybride-tool.png',
+      'lencioni/features/2-integratie-deelnemers.png',
+      'recensies/ctc-training-1-2.jpeg',
+      'recensies/ctc-groep-be-sept-22.webp',
+      'lencioni/features/5-kartonnen-lencioni-piramide.webp',
+      'lencioni/features/6-team-assessment.png',
+      'lencioni/features/7-personal-assessment.png',
+      'lencioni/features/8-lencioni-werkboek.png',
+      'lencioni/features/9-bijlages-na-tweedaagse.png',
+      'lencioni/features/10-opvolgcoaching.webp',
+      'lencioni/features/11-deepdives.png',
+      'lencioni/features/12-vertrouwen.jpg',
+      'lencioni/features/13-respectvolle-conflicten.png',
+      'lencioni/features/14-team-betrokkenheid.png',
+      'lencioni/features/15-aansprakelijkheid.png',
+      'lencioni/features/16-resultaatgerichtheid.png',
+      'lencioni/features/17-bruggen-andere-modellen.webp',
    ]
 
 </script>
 <NavSlider ariaLabel={$LL.sessions.level_1.features_title()}>
    {#each Object.entries($LL.sessions.level_1.features_extended) as [key, feature]}
       <FeatureSlide 
+         title={feature.title.main()}
          modalId={Number(key)+11} 
          imgSrc="{featureImages[Number(key)]}" imgAlt="{feature.title.before()} {feature.title.main()}" 
          class="!h-fit">
@@ -52,10 +53,8 @@
             {title()}&nbsp;
       {/each}
    </span>
-   <Text>
-      {#each Object.entries(feature.content) as [contentKey, content]}
-            {@html content()}
-      {/each}
+   <Text class="prose-li:list-disc prose-ul:ml-4">
+      {@html feature.content()}
    </Text>
    <span slot="extra" class:hidden={!feature.extra}>
          {#if feature.extra}
