543965865eb2a011d1c3e90b14eb733819a3608f
raremiroir <mirostorm@gmail.com>
2023-02-15 12:50:44 +0100

    Add SEO to head

--- src/lib/components/Common/SEO/index.svelte
+++ src/lib/components/Common/SEO/index.svelte
@@ -94,7 +94,24 @@
 		name="robots"
 		content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
 	/>
+	<!-- Canonical link element 
+		tells search engines which URL is the canonical version of the page. 
+		helps prevent duplicate content issues and ensures that the correct page is indexed. -->
 	<link rel="canonical" href={url} />
+	<!-- sitemap link element 
+		tells search engines where to find the sitemap for the website. 
+		Sitemaps are XML files that contain a list of all the pages on the website and their metadata. 
+		used by search engines to index the website and display it in search results. -->
+	<link rel="sitemap" type="application/xml" href="https://samplesite.com/sitemap.xml">
+	<!-- alternate link element 
+		tells search engines where to find the RSS feed for the website. 
+		RSS feeds are XML files that contain a list of the most recent posts on the website. 
+		used by search engines to display the website's content in search results, as well as by RSS readers to display the website's content in a more convenient format. -->
+  	<link rel="alternate" type="application/rss+xml" title="RSS" href="https://samplesite.com/rss.xml">
+	<!-- search link element 
+	   used by browsers to display a search box in the browser's address bar.
+		allows users to search the website directly from the address bar, instead of having to navigate to the search page. -->
+  	<link rel="search" type="application/opensearchdescription+xml" title="Search" href="https://samplesite.com/search.xml">
 </svelte:head>
 
 <OpenGraph {...openGraphProps} />
