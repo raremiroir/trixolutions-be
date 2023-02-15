13641fb19fe91a898f23cf91273e04e50d810b2f
raremiroir <mirostorm@gmail.com>
2023-02-15 16:55:48 +0100

    Switch to lightbox

--- src/lib/components/Content/FeaturesSlider/FeaturesModalContent.svelte
+++ src/lib/components/Content/FeaturesSlider/FeaturesModalContent.svelte
@@ -1,26 +1,11 @@
 <script lang="ts">
-	import { Modal, Image, Text, Title } from "$comp";
-
-   import { breakpoints } from "$src/lib/constants/breakpoints";
+	import { Modal, Image, Text, Title, Lightbox } from "$comp";
 
    export let id:number;
-
-   let innerWidth:number;
-   let ratio = '5:3'
-   $: if (innerWidth < breakpoints.xs) { ratio = "2:1"; }  
-      else if (innerWidth < breakpoints.sm) { ratio = "2:1"; } 
-      else if (innerWidth < breakpoints.md) { ratio = "2:1"; } 
-      else if (innerWidth < breakpoints.lg) { ratio = "3:2"; } 
-      else if (innerWidth < breakpoints.xl) { ratio = "4:3"; } 
-      else if (innerWidth < breakpoints.xxl) { ratio = "3:2"; }
-      else { ratio = "5:3"}
-
    export let imgSrc:string;
    export let imgAlt:string;
 
 </script>
-
-<svelte:window bind:innerWidth={innerWidth} />
 
 <Modal content {id}>
    <Title type="h3" smaller slot="title">
@@ -34,12 +19,10 @@
          </Text>
       </div>
       <div class="col-span-1">
-         <Image 
+         <Lightbox 
             alt={imgAlt}
-            src={imgSrc} 
-            mode="cover" position="top" 
-            ratio="{ratio}"
-            intrinsic="600x400"/>
+            src={imgSrc}
+         />
       </div>
    </div>
 </Modal>
