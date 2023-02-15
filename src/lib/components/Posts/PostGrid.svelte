fb2c98fdd77d3a3895f2f450eb5229bdf7b9a52c
raremiroir <mirostorm@gmail.com>
2023-02-15 17:50:13 +0100

    Edit spacing

--- src/lib/components/Posts/PostGrid.svelte
+++ src/lib/components/Posts/PostGrid.svelte
@@ -1,7 +1,7 @@
 <div class="
       grid 
-      grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
-      gap-3 lg:gap-6 xl:gap-8">
+      grid-cols-1 md:grid-cols-2 lg:grid-cols-3 
+      gap-2 sm:gap-3 md:gap-4 2xl:gap-8">
    
       <slot/>
 
