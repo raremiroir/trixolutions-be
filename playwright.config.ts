27cf3b8ecdc8deca62d9151805110e43e5e9568e
raremiroir <mirostorm@gmail.com>
2023-02-16 21:40:19 +0100

    Config playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,42 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
+import { defineConfig } from '@playwright/test';
+export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
-	}
-};
+	},
+	reporter: [
+		['list'],
+		['html']
+	],
+	use: {
+		headless: false,
+		ignoreHTTPSErrors: true,
+		video: 'on-first-retry',
+		trace: 'retain-on-failure'
+	},
+	projects: [
+		{
+			name: 'chrome-desktop',
+			use: {
+				browserName: 'chromium',
+				viewport: { width: 1280, height: 720 },
+			},
+		},
+		{
+			name: 'firefox-desktop',
+			use: {
+				browserName: 'firefox',
+				viewport: { width: 1280, height: 720 },
+			},
+		},
+		{
+			name: 'webkit-desktop',
+			use: {
+				browserName: 'webkit',
+				viewport: { width: 1280, height: 720 },
+			},
+		},
 
-export default config;
+	],
+});
