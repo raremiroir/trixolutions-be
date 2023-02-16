6fa280e91bfd90a574474db2a9d4edc0c7b3990d
raremiroir <mirostorm@gmail.com>
2023-02-16 23:04:21 +0100

    Add PageTransition component

--- src/lib/components/Common/Wrapper/Main.svelte
+++ src/lib/components/Common/Wrapper/Main.svelte
@@ -1,21 +1,9 @@
-<!-- 
-███    ███  █████  ██ ███    ██ 
-████  ████ ██   ██ ██ ████   ██ 
-██ ████ ██ ███████ ██ ██ ██  ██ 
-██  ██  ██ ██   ██ ██ ██  ██ ██ 
-██      ██ ██   ██ ██ ██   ████ 
--->
 
-
-<!--
-▄▄▄ ▄▄▄ 
- █  █▄▄
- █  ▄▄█
--->
 <script lang="ts">
    // Import Components
    import { Accordeon, AccordeonItem, CtA, Reveal } from "$comp";
    import { Section } from ".";
+	import PageTransition from "../../Utils/PageTransition.svelte";
 
    // Define extra classes
    let klass = '';
@@ -33,46 +21,43 @@
    let active:any = null;
 </script>
 
-<!--
-▄  ▄ ▄▄▄ ▄▄   ▄▄ ▄
-█▄▄█  █  █ █ █ █ █
-█  █  █  █ ▀▄▀ █ █▄▄
--->
-<main 
-   class="
-      overflow-x-hidden w-full mb-20
-      {noMargin ? 'mt-6' : 'mt-24'}
-      {klass}"
->
-      
-   <slot/>
-
-   {#if partnership}
-      <Section name="about-partnership">
-
-         <Reveal>
-            <Accordeon bind:active>
-               <AccordeonItem 
-                  id={1} titleType='h2' titleSmallest 
-                  title="Over Trixolutions en Patrick Lencioni">
-                     Bla bla
-               </AccordeonItem>
-               <AccordeonItem 
-                  id={2} titleType='h2' titleSmallest 
-                  title="Mogelijkheden tot (digitale) Opleiding en Coaching">
-                     Bla bla
-               </AccordeonItem>
-            </Accordeon>
-         </Reveal>
-
-      </Section>
-   {/if}
-
-	{#if cta}
-      <Section name="call-to-action">
-         <Reveal bottom slow>
-            <CtA />
-         </Reveal>
-      </Section>
-   {/if}
-</main>
+<PageTransition>
+   <main 
+      class="
+         overflow-x-hidden w-full mb-20
+         {noMargin ? 'mt-6' : 'mt-24'}
+         {klass}"
+   >
+         
+      <slot/>
+   
+      {#if partnership}
+         <Section name="about-partnership">
+   
+            <Reveal>
+               <Accordeon bind:active>
+                  <AccordeonItem 
+                     id={1} titleType='h2' titleSmallest 
+                     title="Over Trixolutions en Patrick Lencioni">
+                        Bla bla
+                  </AccordeonItem>
+                  <AccordeonItem 
+                     id={2} titleType='h2' titleSmallest 
+                     title="Mogelijkheden tot (digitale) Opleiding en Coaching">
+                        Bla bla
+                  </AccordeonItem>
+               </Accordeon>
+            </Reveal>
+   
+         </Section>
+      {/if}
+   
+      {#if cta}
+         <Section name="call-to-action">
+            <Reveal bottom slow>
+               <CtA />
+            </Reveal>
+         </Section>
+      {/if}
+   </main>
+</PageTransition>
---
+++ src/lib/components/Utils/PageTransition.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { fly, fade } from "svelte/transition";
+
+   const duration = 250
+   const delay = 50
+</script>
+
+<div 
+   in:fade="{{ duration: duration, delay: delay }}"
+   out:fade="{{ duration: duration }}" 
+>
+   <div 
+      in:fly="{{ y: -50, duration: duration, delay: delay }}"
+      out:fly="{{ y: -50, duration: duration }}" 
+      class="">
+      <slot/>
+   </div>
+</div>
