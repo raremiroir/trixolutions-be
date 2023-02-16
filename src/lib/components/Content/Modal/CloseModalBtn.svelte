d873725409f308393b148040f73189954f19c0e7
raremiroir <mirostorm@gmail.com>
2023-02-16 21:38:29 +0100

    Update modal button to actual button component

--- src/lib/components/Content/Modal/CloseModalBtn.svelte
+++ src/lib/components/Content/Modal/CloseModalBtn.svelte
@@ -3,32 +3,25 @@
 
 	import { firstLetterCase } from "$utils";
    
-	import Icon from "@iconify/svelte";
-	import { Tooltip } from "$comp";
+	import { Button, Tooltip } from "$comp";
 
    export let onClick:any;
 
 </script>
-<div class="h-8 w-8">
+<div 
+   on:click={onClick}
+   on:keydown={onClick}   
+   class="h-8 w-8">
    <Tooltip
       color="error"
       flyX={20} flyY={0}
       placement='bottom-1 right-9 '
       title={firstLetterCase($LL.base.btn.close())}>
-      <div 
-         class="w-full h-full"
-         on:click|stopPropagation={onClick}
-         on:keydown={onClick}>
-         <Icon 
-            icon="material-symbols:close-rounded" 
-            class="
-               cursor-pointer
-               rounded-full h-full w-full
-               bg-error/70 hover:bg-error
-               hover:shadow-sm
-               text-white p-1
-               transition-all duration-200 ease-in-out
-               "/>
-      </div>
+
+      <Button
+         ariaLabel="Close"
+         color="error"
+         size="sm" square
+         icon="material-symbols:close-rounded" />
    </Tooltip>
 </div>
