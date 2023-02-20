408fc9238d121f4ecf9305015a82e73fa412b53a
raremiroir <mirostorm@gmail.com>
2023-02-20 01:43:39 +0100

    Integrate BlogPost interface and new import from page.ts

--- src/routes/[...lang=lang]/blog/[post]/+page.svelte
+++ src/routes/[...lang=lang]/blog/[post]/+page.svelte
@@ -1,57 +1,74 @@
 <script lang="ts">
-   // Import data (first priority)
-   export let data;
-   const blogPost = Object(data.post);
-   // console.log(blogPost);
-   
-   // Define current page slug
-   import { currentPageMap } from "$lib/stores";
-   $currentPageMap = [
-      {locale: 'en', slug: `blog/${formatUrl(blogPost.title)}`},
-      {locale: 'fr', slug: `blog/${formatUrl(blogPost.title)}`},
-      {locale: 'nl', slug: `blog/${formatUrl(blogPost.title)}`}
-   ];
-
    // Import components
    import { 
       Navbar, Main, Section, 
       H1, H2, Subheader, Subtitle,
-      Breadcrumbs, Tag, Markdown,
+      Breadcrumbs, Tag, MarkProse,
       Hero, PostSEO, Reveal 
    } from "$comp";
    // Import utils
    import { titleCase, formatDateMonthFull, formatTime, formatUrl } from "$utils";
+   
+   // Import types
+   import type { BlogPost } from "$src/types";
+   import type { PageData } from "./$types";
+
+   // Import data (first priority)
+   export let data:PageData;
+   const post:BlogPost = data.post 
+      ? data.post 
+      : {
+            id: 0, title: "",
+            excerpt: "", content: "",
+            language: "nl",
+            img: { name: "", folder: "", type: "" },
+            updated_at: "", created_at: "",
+            author: { first_name: "", last_name: "" },
+            // timeToRead: 0,
+            // tags: [],
+         };
+   // console.log(post);
+   
+   // Define current page slug
+   import { currentPageMap } from "$lib/stores";
+   $currentPageMap = [
+      {locale: 'en', slug: `blog/${formatUrl(post.title)}`},
+      {locale: 'fr', slug: `blog/${formatUrl(post.title)}`},
+      {locale: 'nl', slug: `blog/${formatUrl(post.title)}`}
+   ];
+
 
    // Format URL
-   const url = formatUrl(blogPost.title);
+   const url = formatUrl(post.title);
+
 
    // SEO
    const seoProps = {
-      language: blogPost.language,
-      title: blogPost.title,
-      description: blogPost.excerpt,
-      summary: blogPost.excerpt,
+      language: post.language,
+      title: post.title,
+      description: post.excerpt,
+      summary: post.excerpt,
       slug: url,
-      published: blogPost.created_at,
-      updated: blogPost.updated_at,
-      cover: `${blogPost.img.folder}/${blogPost.img.name}.${blogPost.img.type}`,
-      author: `${blogPost.author.first_name} ${blogPost.author.last_name}`
+      published: post.created_at,
+      updated: post.updated_at,
+      cover: `${post.img.folder}/${post.img.name}.${post.img.type}`,
+      author: `${post.author.first_name} ${post.author.last_name}`
    } 
 </script>
 
 <PostSEO {...seoProps} />
 <header>
    <Navbar/>
-   <Hero imgSrc="{seoProps.cover}" imgAlt="{blogPost.title}">
+   <Hero imgSrc="{seoProps.cover}" imgAlt="{post.title}">
       <div slot="title">
          <Subheader color="text-gray-200">Trixolutions Blog</Subheader>
-         <H1 color="text-gray-100">{titleCase(blogPost.title)}</H1>
+         <H1 color="text-gray-100">{titleCase(String(post.title))}</H1>
          <div class="flex flex-row justify-between w-full items-center">
             <Subtitle color="text-gray-100/50">
                {seoProps.author}
             </Subtitle>
             <Tag outlined secondary class="float-right !text-gray-50/70 !border-gray-50/70">
-               {formatDateMonthFull(blogPost.created_at)} ({formatTime(blogPost.created_at)})
+               {formatDateMonthFull(seoProps.published)} ({formatTime(seoProps.published)})
             </Tag>
          </div>
       </div>
@@ -62,10 +79,12 @@
    <Breadcrumbs currentIcon="mdi:note-text-outline" beforeIcon="mdi:newspaper-variant-outline"/>
    <Section name="{url}">
       <H2 slot="title">
-         <Reveal>{titleCase(blogPost.title)}</Reveal>
+         <Reveal>{titleCase(post.title)}</Reveal>
       </H2>
       <Reveal>
-         <Markdown content={blogPost.content} />
+         <MarkProse>
+            <svelte:component this={post.content} />
+         </MarkProse>
       </Reveal>
    </Section>
 </Main>
