650c8dabd22f19b4bda522bbe3918f12fd995aca
raremiroir <mirostorm@gmail.com>
2023-02-20 01:43:58 +0100

    Convert to BlogPost interface and import api

--- src/routes/[...lang=lang]/blog/[post]/+page.ts
+++ src/routes/[...lang=lang]/blog/[post]/+page.ts
@@ -1,11 +1,15 @@
 import supabase from '$lib/db'
-import { formatUrl } from '$utils/formatText';
+import type { BlogPost } from '$src/types';
 
 
-export const load = ({ fetch, params }) => {
+export const load = async ({ params }:any) => {
    // console.log(params);
+   
+   const post = await import(`../../../../posts/nl/${params.post}.md`);
+   const { id, title, excerpt } = post.metadata;
+   const content = post.default;
 
-   const fetchPost = async (url) => {
+   const fetchPost = async (id:number) => {
       const {data, error} = await supabase
          .from('blog_posts')
          .select(`
@@ -16,18 +20,45 @@ export const load = ({ fetch, params }) => {
       if (error) {
          throw new Error(error.message)
       } else if (data) {
-         let postData;
+         let postData:BlogPost = {
+            id: 0, title: "",
+            excerpt: "", content: "",
+            language: "nl",
+            img: { name: "", folder: "", type: "" },
+            updated_at: "", created_at: "",
+            author: { first_name: "", last_name: "" },
+            // timeToRead: 0,
+            // tags: [],
+         };
          data.forEach(el => {
-            if (formatUrl(el.title) === url) {
-               postData = el;
-               // console.log(url);
+            if (el.id === id) {
+               postData = {
+                  id: id,
+                  title: title,
+                  excerpt: excerpt,
+                  content: content,
+                  language: el.language,
+                  img: {
+                     name: el.img.name,
+                     folder: el.img.folder,
+                     type: el.img.type
+                  },
+                  updated_at: el.updated_at,
+                  created_at: el.created_at,
+                  author: {
+                     first_name: el.author.first_name,
+                     last_name: el.author.last_name,
+                  },
+                  // timeToRead: 0,
+                  // tags: [],
+               }
             }
          });
          return postData;
       }
-   };
-
+   }
+   
    return {
-      post: fetchPost(params.post)
+      post: fetchPost(id)
    }
 }
