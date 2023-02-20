f16321d8b7a562a2893b382bb1c49fcbffe1bcc5
raremiroir <mirostorm@gmail.com>
2023-02-20 13:43:55 +0100

    Refactor redirection on lang param

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,100 +1,122 @@
 // Import supabase
 import '$lib/db';
 
+// Import svelte items
+import type { Handle, HandleServerError, RequestEvent } from '@sveltejs/kit'
+import { redirect, error } from '@sveltejs/kit';
+import { randomUUID } from 'crypto';
+
 // Import i18n
 import { detectLocale, i18n, isLocale } from '$i18n/i18n-util'
 import { loadAllLocales } from '$i18n/i18n-util.sync'
-import type { Handle, RequestEvent } from '@sveltejs/kit'
 import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors'
 import type { Locales } from './i18n/i18n-types';
+
+// Import sentry
+import * as Sentry from '@sentry/node'
+import { BrowserTracing } from '@sentry/tracing';
 
 
 // Define all locales
 const myLocales = ['nl', 'fr', 'en']
 // Define main (first) slugs in urls for each locale
 const mainSlugs = {
-	nl: ['open-sessies', 'contact', 'blog', 'over-ons', 'referenties'],
-	fr: ['sessions-ouvertes', 'a-propos', 'references'],
-	en: ['open-sessions', 'about-us']
+	"nl": ['open-sessies', 'contact', 'blog', 'over-ons', 'referenties'],
+	"fr": ['sessions-ouvertes', 'a-propos', 'references'],
+	"en": ['open-sessions', 'about-us']
 }
+
+const allSlugs = Object.values(mainSlugs).flat();
 
 // Load locales
 loadAllLocales()
 const L = i18n()
 
-export const handle: Handle = async ({ event, resolve }) => {
-		// Define lang according to params
-		let lang = event.params.lang as Locales;
-		// Define rest of path
-		let subPath;
-		let rest;
-		if (lang) {
-			[, , subPath, ...rest] = event.url.pathname.split('/')
-		} else {
-			[, subPath, ...rest] = event.url.pathname.split('/')
-		}
-		// Define locale as lang
-		let locale = lang as Locales;
-		// If lang is invalid, get preferred lang
-		if (!lang || !isLocale(lang) || lang === undefined) {
-	
-			const restString = [...rest].join('/')
-	
-			locale = isLocale(lang) ? (lang as Locales) : getPreferredLocale(event)
-			// console.log('preferred locale: ', locale);
-			let new_path = `/${locale}`
-	
-			// Check if subpath exists
-			if (subPath) {
-				// Check if subpath has a dutch string
-				if (mainSlugs.nl.includes(subPath)) {
-					locale = 'nl';
-					lang = 'nl'
-				// Check if subpath has a french string
-				} else if (mainSlugs.fr.includes(subPath)) {
-					locale = 'fr';
-					lang = 'fr'
-				// Check if subpath has an english string
-				} else if (mainSlugs.en.includes(subPath)) {
-					locale = 'en';
-					lang = 'en'
-				// If no match, redirect to home page with preferred locale
-				} else {
-					return new Response(null, {
-						status: 302,
-						headers: { Location: `/${locale}` },
-					})
-				}
-				event.params.lang = lang;
-				// If the reststring is empty, dont append it, otherwise append it to the new path
-				if (restString !== ('/' || '')) {
-					new_path = `/${locale}/${subPath}/${restString}`
-				} else {
-					new_path = `/${locale}/${subPath}`
-				}
-			}
-			
-			// Redirect to new path
-			return new Response(null, {
-						status: 301,
-						headers: { Location: `${new_path}` },
-					})
-		}
-		// if slug is not a locale, use base locale (e.g. api endpoints)
-		const LL = L[locale]
-	
-		// bind locale and translation functions to current request
-		event.locals.locale = locale
-		event.locals.LL = LL
-	
-		// replace html lang attribute with correct language
-		return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', locale) })
-}
-
+// Get preferred locale function
 const getPreferredLocale = ({ request }: RequestEvent) => {
 	// detect the preferred language the user has configured in his browser
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
 	const acceptLanguageDetector = initAcceptLanguageHeaderDetector(request)
 
 	return detectLocale(acceptLanguageDetector)
+}
+
+// Initialize Sentry
+Sentry.init({
+	dsn: "https://60379ddfe2fc4d5b867bba60a207fcab@o4504485227659264.ingest.sentry.io/4504712253014017",
+	integrations: [new BrowserTracing],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// Adjust this value in production
+   tracesSampleRate: 1.0,
+});
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const params = event.params;
+	if (event.url.pathname === "//") {
+		throw redirect(301, `${event.url.origin}`);
+	}
+	const path = event.url.pathname;
+
+	const splitPath = path.split('/');
+	const firstParam = splitPath[1];
+	// console.log(splitPath, firstParam)
+
+	let locale:Locales = 'nl'
+	let restPath = '';
+
+	if (isLocale(firstParam) || (allSlugs.includes(firstParam) && firstParam !== '') || firstParam === '' || firstParam === '404') {
+		if (isLocale(firstParam)) {
+			// Define lang according to params
+			locale = firstParam as Locales;
+			restPath = splitPath.length > 2 ? splitPath.slice(2).join('/') : '';
+		}
+		else if(allSlugs.includes(firstParam)  && firstParam !== ''){
+			Object.keys(mainSlugs).forEach(key => {
+				mainSlugs[key].forEach((slug:string) => {
+					if (firstParam === slug) {
+						locale = `${key}` as Locales;
+						restPath = splitPath.length > 2 ? splitPath.slice(2).join('/') : '';
+						// Redirect to new path
+						throw redirect(301, `/${locale}/${firstParam}/${restPath}`);
+					}
+				})
+			})
+		} else if (firstParam === '') {
+			locale = getPreferredLocale(event);
+			// Redirect to new path
+			throw redirect(301, `/${locale}`);
+		} else if ( firstParam === '404' ) {
+			locale = getPreferredLocale(event);
+			// Redirect to new path
+			throw redirect(301, `/${locale}/404`);
+		} else {
+			throw redirect(301, `/404`);
+		}
+
+		const LL = L[locale];
+		event.locals.locale = locale;
+		event.locals.LL = LL;
+		
+		return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', locale) });
+
+	} else if (
+		path.startsWith('/api/') ||
+		path === ('/sitemap.xml') ||
+		path.startsWith('/admin')
+	){
+		return await resolve(event);
+	} else {
+		throw redirect(302, '/404');
+	}
+}
+
+export const handleError:HandleServerError = ({ error, event }) => {
+	const errorId = randomUUID();
+	Sentry.captureException(error, { event, errorId });
+	return {
+		message: `Whoops! 🥺`,
+		errorId,
+	}
 }
