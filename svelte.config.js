3977fb437ad6baf433db4a3f73d2aad4b5119bc7
raremiroir <mirostorm@gmail.com>
2023-02-20 01:38:33 +0100

    Remove marked, add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-      preprocess({
-        postcss: true
-      })
-   ],
-
-	kit: {
-		adapter: adapter(),
+   kit: {
+      adapter: adapter(),
       alias: {
          $src: 'src',
          $lib: 'src/lib',
@@ -24,7 +19,20 @@ const config = {
          $route: 'src/routes',
       },
       // csp: { mode: 'auto' }
-	}
+   },
+   extensions: [
+      '.svelte',
+      '.md'
+   ],
+	preprocess: [
+      preprocess({
+        postcss: true,
+      }),
+      mdsvex({
+         extensions: ['.md'],
+         smartypants: true,
+      })
+   ]
 };
 
 export default config;
