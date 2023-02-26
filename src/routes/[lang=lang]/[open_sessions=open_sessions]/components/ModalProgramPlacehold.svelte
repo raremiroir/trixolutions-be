37a2159b2d1cc849572900785f6f165ffea4780a
raremiroir <mirostorm@gmail.com>
2023-02-26 22:47:09 +0100

    Remove unused props

--- src/routes/[lang=lang]/[open_sessions=open_sessions]/components/ModalProgramPlacehold.svelte
+++ src/routes/[lang=lang]/[open_sessions=open_sessions]/components/ModalProgramPlacehold.svelte
@@ -20,18 +20,18 @@
       </TabList>
       <TabPanel>
          <List>
-            <ListItem large>Je ontvangt een schriftelijke intake om je verwachtingen goed in kaart te brengen.</ListItem>
-            <ListItem large>Je ontvangt een e-mail + link om het Lencioni “Team Assessment” in te vullen.</ListItem>
+            <ListItem>Je ontvangt een schriftelijke intake om je verwachtingen goed in kaart te brengen.</ListItem>
+            <ListItem>Je ontvangt een e-mail + link om het Lencioni “Team Assessment” in te vullen.</ListItem>
          </List>
       </TabPanel>
       <TabPanel>
          <List>
-            <ListItem large><strong>Wie is Patrick Lencioni</strong> en zijn Table Group?</ListItem>
-            <ListItem large>Lencioni's meest succesvolle boeken, inzichten en verbanden met elkaar.</ListItem>
-            <ListItem large>Ons Trixolutions lidmaatschap met The Table Group – wat houdt dat juist in?</ListItem>
-            <ListItem large>Gedachtengoed van de Lencioni Piramide uit <em>“De 5 Frustraties van Teamwork”</em>.</ListItem>
+            <ListItem><strong>Wie is Patrick Lencioni</strong> en zijn Table Group?</ListItem>
+            <ListItem>Lencioni's meest succesvolle boeken, inzichten en verbanden met elkaar.</ListItem>
+            <ListItem>Ons Trixolutions lidmaatschap met The Table Group – wat houdt dat juist in?</ListItem>
+            <ListItem>Gedachtengoed van de Lencioni Piramide uit <em>“De 5 Frustraties van Teamwork”</em>.</ListItem>
             <br/>
-            <ListItem large>Tussentijdse oefeningen tegen dag 2.</ListItem>
+            <ListItem>Tussentijdse oefeningen tegen dag 2.</ListItem>
          </List>
          <hr/>
          <div class="grid grid-cols-2 gap-4">
@@ -39,14 +39,14 @@
                <H5 small>Piramide Laag 1</H5>
                <List>
                   <Subtitle italic>"Vertrouwen"</Subtitle>
-                  <ListItem large>Welke Lencioni-oefeningen zijn er beschikbaar voor een teamcoach en hoe deze in te zetten?</ListItem>
+                  <ListItem>Welke Lencioni-oefeningen zijn er beschikbaar voor een teamcoach en hoe deze in te zetten?</ListItem>
                </List>
             </div>
             <div class="col-span-1">
                <H5 small>Piramide Laag 2</H5>
                <List>
                   <Subtitle italic>"Respectvolle Conflicten"</Subtitle>
-                  <ListItem large>Welke Lencioni-oefeningen zijn er beschikbaar voor een teamcoach en hoe deze in te zetten?</ListItem>
+                  <ListItem>Welke Lencioni-oefeningen zijn er beschikbaar voor een teamcoach en hoe deze in te zetten?</ListItem>
                </List>
             </div>
          </div>
@@ -54,9 +54,9 @@
       <TabPanel></TabPanel>
       <TabPanel>
          <List>
-            <ListItem large>Je kan met de trainer (Tom) een uur persoonlijke opvolgcoaching inplannen.</ListItem>
-            <ListItem large>Je krijgt gedurende 12 maanden, elke maand, een Lencioni Newsletter met nieuwe en extra oefeningen, samenvattingen van Lencioni Podcasts en inzichten uit de Lencioni CAPA PRO Academy.   </ListItem>
-            <ListItem large>Je kan het volledige digitale Lencioni Team Assessment 1 keer inzetten, onder je eigen logo, ter waarde van 490€.</ListItem>
+            <ListItem>Je kan met de trainer (Tom) een uur persoonlijke opvolgcoaching inplannen.</ListItem>
+            <ListItem>Je krijgt gedurende 12 maanden, elke maand, een Lencioni Newsletter met nieuwe en extra oefeningen, samenvattingen van Lencioni Podcasts en inzichten uit de Lencioni CAPA PRO Academy.   </ListItem>
+            <ListItem>Je kan het volledige digitale Lencioni Team Assessment 1 keer inzetten, onder je eigen logo, ter waarde van 490€.</ListItem>
             
          </List>
       </TabPanel>
