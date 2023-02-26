db98dbb9da913605a1f9ac2465c1eff7ad617522
raremiroir <mirostorm@gmail.com>
2023-02-26 19:19:45 +0100

    Update utils

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,30 +1,35 @@
-// Hello world
-export { firstLetterCase as firstLetterCase } from './formatText';
-// Hello World
-export { titleCase as titleCase } from './formatText';
-// hello-world
-export { formatUrl as formatUrl } from './formatText';
+// Formatters
+export { generateUsername as generateUsername } from './formatters/formatText';
+export { firstLetterCase as firstLetterCase } from './formatters/formatText';             // Hello world
+export { titleCase as titleCase } from './formatters/formatText';                         // Hello World
+export { formatUrl as formatUrl } from './formatters/formatText';                         // hello-world
+export { formatPrice as formatPrice } from './formatters/formatPrice';                    // 1,500.99
+export { formatYear as formatYear } from './formatters/formatDateTime'                    // 2023
+export { formatMonthShort as formatMonthShort } from './formatters/formatDateTime'        // jan
+export { formatDateMonthFull as formatDateMonthFull } from './formatters/formatDateTime'  // 10 Januari
+export { formatDateFull as formatDateFull } from './formatters/formatDateTime'            // 10/01/2023
+export { formatDateShort as formatDateShort } from './formatters/formatDateTime'          // 10/01
+export { formatTime as formatTime } from './formatters/formatDateTime'                    // 15:45
 
-export { generateUsername as generateUsername } from './formatText';
-
-// 1,500.99
-export { formatPrice as formatPrice } from './formatPrice';
-
-// 2023
-export { formatYear as formatYear } from './formatDateTime'
-// jan
-export { formatMonthShort as formatMonthShort } from './formatDateTime'
-// 10 Januari
-export { formatDateMonthFull as formatDateMonthFull } from './formatDateTime'
-// 10/01/2023
-export { formatDateFull as formatDateFull } from './formatDateTime'
-// 10/01
-export { formatDateShort as formatDateShort } from './formatDateTime'
-// 15:45
-export { formatTime as formatTime } from './formatDateTime'
-
+// I18n
 export { replaceLocaleInUrl as replaceLocaleInUrl } from './i18n'
 export { changeUrlToLocale as changeUrlToLocale } from './i18n'
 export { processPathname as processPathname } from './i18n'
 
+// Blog
 export { fetchMarkdownPosts as fetchMarkdownPosts } from './blog';
+
+// Form
+export { createYupSchema as createYupSchema } from './form'
+export { declareFormValues as declareFormValues } from './form'
+
+// Mailgun
+export { mailToAdmin as mailToAdmin } from './mailgun'
+
+// Supabase Queries
+export { dbInsert as dbInsert } from './query'
+
+// Checkers
+export { isObjEmpty as isObjEmpty } from './base/checkers'
+export { isObjEmptyAny as isObjEmptyAny } from './base/checkers'
+
