066923431ed4a7e9c9d3fa686cedb3c4bd0d9825
raremiroir <mirostorm@gmail.com>
2023-02-27 15:24:23 +0100

    Temporarily disable mailchimp

--- src/routes/api/registerMailchimp/+page.server.ts
+++ src/routes/api/registerMailchimp/+page.server.ts
@@ -1,18 +1,18 @@
-import mailchimp from '$lib/services/mailchimp';
+// import mailchimp from '$lib/services/mailchimp';
 
-export async function _post({ body }:any) {
-    try {
-        let data = await JSON.parse(body).catch((err:any) => {
-            console.log(err);
-        })
-        let result = await mailchimp.registerEmail(data.email).catch((err:any) => {
-            console.log(err);
-        });
-        return {
-            status: result.status,
-            body: result
-        };
-    } catch (error) {
-        console.error(error);
-    }
-}
+// export async function _post({ body }:any) {
+//     try {
+//         let data = await JSON.parse(body).catch((err:any) => {
+//             console.log(err);
+//         })
+//         let result = await mailchimp.registerEmail(data.email).catch((err:any) => {
+//             console.log(err);
+//         });
+//         return {
+//             status: result.status,
+//             body: result
+//         };
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
