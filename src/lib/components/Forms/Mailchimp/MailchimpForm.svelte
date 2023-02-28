88fb72c0ac0a67614214e1bcd164adc3c86bd14d
raremiroir <mirostorm@gmail.com>
2023-02-28 17:32:57 +0100

    Edit mailchimp form with new scripts

--- src/lib/components/Forms/Mailchimp/MailchimpForm.svelte
+++ src/lib/components/Forms/Mailchimp/MailchimpForm.svelte
@@ -1,95 +1,67 @@
 <script lang="ts">
+   import type { ChosenInputs } from "../types"
 
-   import { Button, Tooltip } from '$comp';
-   import { RowWrap, MessageSentAlert } from '../FormUtils';
-   import FormInput from '../FormInput/index.svelte'
-   import { Confetti } from 'svelte-confetti'
+   import { Alert, FormBase, MessageSentAlert } from "$comp"
 
-   // Import form utils
-   import { createForm } from "svelte-forms-lib";
-   import * as yup from 'yup';
-   // Import i18n
    import LL from "$i18n/i18n-svelte";
-   // Import utils
-	import { firstLetterCase, titleCase } from "$lib/utils";
+
+   import { registerMailchimp } from '$utils'
+	import type { Locale } from "typesafe-i18n/types/runtime/src/core.mjs";
 	import { fade } from "svelte/transition";
 
-   let initValues = {
-      email: '',
-   }
-   let validationSchema = yup.object().shape({
-      email: yup
-         .string()
-         .email($LL.base.validation.email_error())
-         .required($LL.base.validation.required({ item: titleCase($LL.base.form.email()) })),
-   })
-
-   let messageSent = false;
-
-   const subscribeMailchimp = async() => {
-      let body = {
-         email: $form.email
-      };
-      let result = await fetch('/api/registerMailchimp', {
-         method: 'post',
-         body: JSON.stringify(body)
-      });
-      const registerEmailResponse = await result.json();
-      if (registerEmailResponse.status === 200) {
-         $form.email = '';
-      }
+   let inputItems: ChosenInputs = {
+      name: { enabled: true, required: true },
+      email: { enabled: true, required: true }
    }
 
-   const {
-      form, errors, state, isValid,
-      isSubmitting, isValidating,
-      handleChange, handleSubmit
-   } = createForm({
-      initialValues: initValues,
-      validationSchema: validationSchema,
-      onSubmit: values => {
-         subscribeMailchimp()
-      }
-   })
+   let message = ''
+   let error = ''
+   const register = async (first_name:string, last_name:string, email:string, lang:Locale) => {
+      try {
+         const res = await registerMailchimp({ first_name, last_name, email, lang });
+         const data:any = await res.data;
+         if (data.success) {
+            message = $LL.base.newsletter.success.subscribed({ name: first_name });
+         } else {
+            error = `${JSON.parse(data.data.response.text).detail}`;
 
-   $: empty = !$form.email;
-   const resetForm = () => {$form.email = ''; messageSent = false}
+            if (error.includes("already a list member")) {
+               error = $LL.base.newsletter.validation.already_member({email: email});
+            } else if (error.includes("looks fake or invalid")) {
+               error = $LL.base.newsletter.validation.looks_invalid({email: email});
+            }
+         }
+      } catch (err:any) {
+         error = $LL.base.newsletter.validation.failed();
+         ;
+      }
+   }
+   $: message;
+   $: error;
 </script>
 
-<div class="w-full flex items-center justify-center">
-   <form on:submit|preventDefault={handleSubmit}
-         class="flex flex-col gap-4 overflow-y-hidden w-3/4">
-   
-      <RowWrap>
-         <FormInput 
-               name="email"
-               label="{titleCase($LL.base.form.email())}"
-               type="email"
-               on:change={handleChange}
-               bind:value={$form.email}
-               bind:errors={$errors.email}
-               required/>
-      </RowWrap>
-   
-      <RowWrap>
-         <Tooltip 
-            title={!$isValid || empty
-                     ? $LL.base.form.content.fill_out_all() 
-                     :  firstLetterCase($LL.base.form.content.send_msg())} 
-            color={!$isValid || empty ? 'error' : 'primary'}
-            placement="{!$isValid || empty ? 'left-0 bottom-12' : '-right-32 bottom-2'}"
-            flyY={!$isValid || empty ? 12 : 0} flyX={!$isValid || empty ? 0 : -16}>
-            <Button 
-               ariaLabel="Subscribe to newsletter"
-               disabled={!$isValid || empty} 
-               type="submit">
-               {#if $isSubmitting}
-                  <Confetti amount={70} x={[-0.5, 0.5]} y={[-0.5, -0.5]} colorArray={["#0b3259", "#fb5607", "#195693", "#d1d1ce", "#3a86ff"]} />
-               {/if}
-               Subscribe to newsletter
-            </Button>
-         </Tooltip>
-      </RowWrap>
-   
-   </form>
-</div>
+<FormBase
+   {inputItems} extraSuccess={message !== ''}
+   submitText={$LL.base.newsletter.btn()}
+   submitAction={(values) => {
+      const first_name = values.first_name;
+      const last_name = values.last_name;
+      const email = values.email;
+      const lang = values.language;
+      register(first_name, last_name, email, lang);
+   }}
+>
+   <MessageSentAlert slot="success">
+      {message}
+   </MessageSentAlert>
+
+   <div slot="errors" class="">
+      {#if error !== ''}
+         <span
+            class="text-error font-bold italic text-md" 
+            transition:fade={{duration: 200}}>
+               {error}
+         </span>
+      {/if}
+   </div>
+</FormBase>
