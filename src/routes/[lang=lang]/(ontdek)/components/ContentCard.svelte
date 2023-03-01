4f4189e3a6ff7a5020224ba7f1bb0154f7b607be
raremiroir <mirostorm@gmail.com>
2023-03-01 22:30:18 +0100

    Add altBg prop

--- src/routes/[lang=lang]/(ontdek)/components/ContentCard.svelte
+++ src/routes/[lang=lang]/(ontdek)/components/ContentCard.svelte
@@ -18,7 +18,7 @@
    // export let altBg = false
 </script>
 
-<CardBase {title} {hoverFx} class={klass} img=''>
+<CardBase {title} {hoverFx} class={klass} img='' altBg>
    <H3 slot="title" smallest 
        class="text-center w-full border-b-2 border-primary/50">
        {#if id}<span class="text-4xl">{id}</span><br/>{/if}
