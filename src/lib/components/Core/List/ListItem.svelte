ea34b2b04427368679387f6d80e92b191586dc5f
raremiroir <mirostorm@gmail.com>
2023-03-01 22:28:15 +0100

    custom list style

--- src/lib/components/Core/List/ListItem.svelte
+++ src/lib/components/Core/List/ListItem.svelte
@@ -8,7 +8,7 @@
    export let small = false;
 
    // Custom icon?
-   export let icon = '';
+   export let icon = 'default';
 
    // Indent the list item ?
    export let indent = false;
@@ -17,37 +17,41 @@
 
    // Alt bg?
    export let alt = false;
+   // Transparent bg?
+   export let transp = false;
 </script>
 
 <li class="
       w-full
-      flex flex-row items-center justify-start gap-4 sm:gap-6
-      {icon ? 'list-none' : 'list-disc' }
+      flex flex-row items-center justify-start gap-0
+      list-none
       {indent ? 'pl-8' : 'pl-1 xs:pl-2 sm:pl-4 py-1'}
       font-body leading-relaxed font-normal tracking-tight
       { small ? 'text-base' : 'text-lg' }
       text-gray-700 
-      { alt ? 'bg-gray-50' : 'bg-white'}
+      { alt ? 'bg-gray-50' : transp ? 'bg-transparent' : 'bg-white'}
       {klass}">
 
    {#if icon}
    <div class="w-1/12">
       {#if icon === 'check'}
          <Icon 
-         slot="icon" 
-         class="w-7 h-6 bg-success-d/30 rounded-full text-success-d" 
+         class="w-6 h-6 bg-success-d/30 rounded-full text-success-d" 
          icon="material-symbols:check-circle-outline-rounded" />
       {:else if icon === 'cancel'}
          <Icon 
-         slot="icon" 
-         class="w-7 h-6 bg-error/30 rounded-full text-error-d" 
+         class="w-6 h-6 bg-error/30 rounded-full text-error-d" 
          icon="material-symbols:cancel-outline-rounded" />
-      {:else if icon !== ''}
+      {:else if icon === 'default'}
+         <Icon 
+            class="w-3 h-3  p-0 border-primary-d3 bg-transparent rounded-full text-primary-d3" 
+            icon="ph:octagon-duotone" />   
+      {:else}
          <slot name="icon" />
       {/if}
    </div>
    {/if}
-   <div class="{icon ? 'w-11/12' : 'w-full'} {disabled ? 'text-gray-500 line-through' : ''}">
+   <div class="w-11/12 {disabled ? 'text-gray-500 line-through' : ''}">
       <slot/>
    </div>
 </li>
