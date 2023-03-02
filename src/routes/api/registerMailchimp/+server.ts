04b7d1413f19be1cba271b9d5cb2bfa88a48a073
raremiroir <mirostorm@gmail.com>
2023-03-02 12:45:01 +0100

    Use functions directly in API with env vars

--- src/routes/api/registerMailchimp/+server.ts
+++ src/routes/api/registerMailchimp/+server.ts
@@ -1,4 +1,15 @@
-import { registerEmail } from '$lib/services/mailchimp';
+import client from '@mailchimp/mailchimp_marketing'
+
+import { 
+    MAILCHIMP_API_KEY,
+    MAILCHIMP_LIST_ID,
+    MAILCHIMP_SERVER
+} from '$env/static/private';
+
+client.setConfig({
+    apiKey: MAILCHIMP_API_KEY,
+    server: MAILCHIMP_SERVER
+})
 
 export async function POST ({ request }: { request: Request }) {
     const { first_name, last_name, email, lang } = await request.json();
@@ -16,5 +27,40 @@ export async function POST ({ request }: { request: Request }) {
             JSON.stringify({ message: 'Failed to register email with API', err }),
             { status: 500 }
         )
+    }
+}
+
+
+const registerEmail = async (first_name:string, last_name:string, email:string, lang:string = 'nl' ) => {
+    try {
+        // console.log(`email: ${email}, first_name: ${first_name}, last_name: ${last_name}, lang: ${lang}`);
+        const res = await client.lists.addListMember(MAILCHIMP_LIST_ID, {
+            email_address: email,
+            language: lang,
+            status:'subscribed',
+            merge_fields: {
+                FNAME: first_name,
+                LNAME: last_name
+            },
+            tags: [
+                lang === 'nl' ? 'BE-2023-nl'
+                : lang === 'en'? 'BE-2023-en'
+                : lang === 'fr'? 'BE-2023-fr'
+                : 'BE-2023'
+            ]
+        })
+
+        return {
+            success: true,
+            message: 'Success',
+            data: res
+        };
+
+    } catch (err:any) {
+        return {
+            success: false,
+            message: err.message,
+            data: err
+        }
     }
 }
--- src/routes/api/sendEmail/+server.ts
+++ src/routes/api/sendEmail/+server.ts
@@ -1,4 +1,29 @@
-import { sendEmail } from "$lib/services/mailgun";
+import Mailgun from 'mailgun.js'
+import formData from 'form-data'
+const mailgun = new Mailgun(formData);
+
+import {
+   MAILGUN_API_KEY,
+   MAILGUN_BASE_URL,
+   MAILGUN_DOMAIN
+} from "$env/static/private"
+
+const mg = mailgun.client({
+   username: 'api',
+   url: MAILGUN_BASE_URL,
+   key: MAILGUN_API_KEY,
+})
+
+const sender = {
+   name: 'Trixolutions Admin',
+   email: ''
+}
+
+const recipients = [
+   'mirostorm@gmail.com',
+   'miro.storm@trixolutions.be',
+]
+const cc = 'miro.polfliet@student.kdg.be'
 
 export async function POST ({ request }: { request: Request }) {
    const { subject, message } = await request.json();
@@ -21,3 +46,20 @@ export async function POST ({ request }: { request: Request }) {
 
 }
 
+const sendEmail = async(subject:string, message:string) => {
+   try {
+      const data = {
+         from: `${sender.name} <noreply@${MAILGUN_DOMAIN}>`,
+         to: recipients,
+         cc: cc,
+         subject: subject,
+         text: message,
+         html: `<html>${message}</html>`,
+      }
+
+      const res = mg.messages.create(MAILGUN_DOMAIN, data)
+      // console.log(res);
+   } catch(err) {
+      console.error(err);
+   };
+}
