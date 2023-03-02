0ba4042f5d8d2d9b79974d5a69e70de0e8d1d233
raremiroir <mirostorm@gmail.com>
2023-03-02 02:04:36 +0100

    Handle error on client side hooks

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,1 +1,11 @@
 import '$lib/db';
+import type { HandleClientError } from '@sveltejs/kit';
+
+export const handleError:HandleClientError = async ({ error, request }) => {
+   if (error.status === 404) {
+      return {
+         status: 404,
+         body: 'Whoops! 🥺'
+      };
+   }
+}
