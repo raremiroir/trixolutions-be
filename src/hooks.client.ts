5ae139101fc7f4aa81db6e6a19a94d1c99c87697
raremiroir <mirostorm@gmail.com>
2023-03-02 02:10:14 +0100

    Merge from features branch

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,11 +1,10 @@
 import '$lib/db';
 import type { HandleClientError } from '@sveltejs/kit';
 
-export const handleError:HandleClientError = async ({ error, request }) => {
-   if (error.status === 404) {
-      return {
-         status: 404,
-         body: 'Whoops! 🥺'
-      };
-   }
+export const handleError:HandleClientError = async ({ error, event }) => {
+   console.log(error)
+   return {
+      status: 404,
+      message: 'Whoops! 🥺'
+   };
 }
