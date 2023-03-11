a3f0f452c1ed5b11d4cb85a6aaccfcc24a92f1a4
raremiroir <mirostorm@gmail.com>
2023-03-12 00:59:25 +0100

    Export new utils

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -4,6 +4,9 @@ export { titleCase as titleCase } from './formatters';                         /
 export { formatUrl as formatUrl } from './formatters';                         // hello-world
 export { formatPrice as formatPrice } from './formatters';
 
+// Regex formatters
+export { formatRegex as formatRegex } from './formatters';
+
 // I18n
 export { replaceLocaleInUrl as replaceLocaleInUrl } from './i18n'
 export { changeUrlToLocale as changeUrlToLocale } from './i18n'
@@ -19,6 +22,7 @@ export { registerMailchimp as registerMailchimp } from './mail'
 // Supabase Queries
 export { dbSelect as dbSelect } from './query'
 export { dbSelectOrder as dbSelectOrder } from './query'
+export { dbSelectFilter as dbSelectFilter } from './query'
 export { dbInsert as dbInsert } from './query'
 
 // Checkers
@@ -27,3 +31,6 @@ export { isObjEmptyAny as isObjEmptyAny } from './base/checkers'
 
 // Actions
 export { tooltip as tooltip } from './actions/tooltip'
+
+// Params
+export { getParamValues as getParamValues } from './formatters'
