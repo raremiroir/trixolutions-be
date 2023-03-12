5002c44e81558c3a9fea1881bb762f3a749f8a8a
raremiroir <mirostorm@gmail.com>
2023-03-12 01:18:09 +0100

    Remove unnecessary i18n call

--- src/routes/[lang=lang]/(main)/[about=about]/+page.ts
+++ src/routes/[lang=lang]/(main)/[about=about]/+page.ts
@@ -1,6 +1,4 @@
 import type { PageLoad } from './$types'
-import LL, { setLocale } from '$i18n/i18n-svelte'
-import { get } from 'svelte/store'
 import { error } from '@sveltejs/kit';
 import { isLocale } from '$i18n/i18n-util';
 import { getParamValues } from '$lib/utils';
@@ -12,14 +10,6 @@ export const load: PageLoad = async ({ parent, params }) => {
 
 	const paramValues = await getParamValues(pageName);
 	if (params.lang in paramValues && isLocale(params.lang) && paramValues[params.lang] === params[pageName]) {
-		// wait for `+layout.ts` to load dictionary and pass locale information
-		const { locale } = await parent()
-	
-		// if you need to output a localized string in a `load` function,
-		// you always need to call `setLocale` right before you access the `LL` store
-		setLocale(locale)
-		// get the translation functions value from the store
-		const $LL = get(LL)
 		return
 	}
 
