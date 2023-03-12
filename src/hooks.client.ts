e77506ea307996cd9b82162f222c55889201e67d
raremiroir <mirostorm@gmail.com>
2023-03-12 01:29:29 +0100

    Add error message

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -5,6 +5,6 @@ export const handleError:HandleClientError = async ({ error, event }) => {
    console.error(error)
    return {
       status: 404,
-      message: `Whoops! 🥺 ${error}`
+      message: `Whoops! 🥺`
    };
 }
